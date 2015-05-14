5139cc71859ce8d6b56a26b33266313819802c1b
mgechev <mgechev@gmail.com>
2015-05-14 13:03:23 +0300

    Update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,6 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
 var traceur = require('gulp-traceur');
-var NG_PATH = './node_modules/angular2/es6/dev/';
-var RTT_PATH = './node_modules/rtts_assert/';
-
-gulp.task('build:ng', shell.task([
-    'npm install',
-    './es5build.js -d ../../../../lib/angular'
-  ], {
-    cwd: NG_PATH
-  })
-);
-
-gulp.task('build:rtts', shell.task([
-    'npm install',
-    './es6/es5build.js -d ./../../lib/rtts_assert'
-  ], {
-    cwd: RTT_PATH
-  })
-);
-
-gulp.task('build:angular', ['build:ng', 'build:rtts']);
 
 gulp.task('build', function () {
   return gulp.src('./js/**/*.js')
