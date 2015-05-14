df6f6fda0c47685b9beb2c864e417c308cdf2b54
mgechev <mgechev@gmail.com>
2015-05-14 18:10:42 +0300

    Fix #2

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ gulp.task('clean', function (done) {
   del(['dev'], done);
 });
 
-gulp.task('angular2', function () {
+gulp.task('build:angular2', function () {
   var builder = new Builder({
     paths: {
       'angular2/*': 'node_modules/angular2/es6/prod/*.es6',
@@ -21,17 +21,21 @@ gulp.task('angular2', function () {
       }
     }
   });
-  return builder.build('angular2/angular2', './lib/angular2/angular2.js', {});
+  return builder.build('angular2/angular2', './lib/angular2.js', {});
 });
 
-gulp.task('traceur', function () {
-  gulp.src('./node_modules/gulp-traceur/node_modules/traceur/bin/traceur-runtime.js')
-    .pipe(gulp.dest('./lib/traceour'));
-});
-
-gulp.task('zone', function () {
-  gulp.src('./node_modules/angular2/node_modules/zone.js/dist/zone.js')
-    .pipe(gulp.dest('./lib/zone.js/'));
+gulp.task('build:lib', ['build:angular2'], function () {
+  gulp.src([
+    './node_modules/gulp-traceur/node_modules/traceur/bin/traceur-runtime.js',
+    './node_modules/angular2/node_modules/zone.js/zone.js',
+    './node_modules/es6-module-loader/dist/es6-module-loader-sans-promises.js',
+    './node_modules/es6-module-loader/dist/es6-module-loader-sans-promises.js.map',
+    './node_modules/reflect-metadata/Reflect.js',
+    './node_modules/reflect-metadata/Reflect.js.map',
+    './node_modules/systemjs/dist/system.js',
+    './node_modules/systemjs/dist/system.js.map'
+  ])
+  .pipe(gulp.dest('./lib'));
 });
 
 gulp.task('build', function () {
