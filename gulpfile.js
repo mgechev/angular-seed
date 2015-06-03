b26231a17558302c8717a78832f0b616108f3169
ludohenin <ludovic.henin@yahoo.com>
2015-06-03 15:29:58 +0200

    Add build.prod and asset tags injection

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 'use strict';
 
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var filter = require('gulp-filter');
+var inject = require('gulp-inject');
+var plumber = require('gulp-plumber');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+
 var del = require('del');
 var Builder = require('systemjs-builder');
 var ts = require('gulp-typescript');
-var plumber = require('gulp-plumber');
-var sourcemaps = require('gulp-sourcemaps');
 var runSequence = require('run-sequence');
+var series = require('stream-series');
 
 var http = require('http');
 var connect = require('connect');
@@ -22,7 +28,9 @@ var PATH = {
     all: 'dist',
     dev: {
       all: 'dist/dev',
-      lib: 'dist/dev/lib'
+      lib: 'dist/dev/lib',
+      ng2: 'dist/dev/lib/angular2.js',
+      router: 'dist/dev/lib/router.js'
     },
     prod: {
       all: 'dist/prod',
@@ -30,20 +38,21 @@ var PATH = {
     }
   },
   src: {
+    // Order is quite important here for the HTML tag injection.
     lib: [
       './node_modules/angular2/node_modules/traceur/bin/traceur-runtime.js',
-      './node_modules/angular2/node_modules/zone.js/dist/zone.js',
       './node_modules/es6-module-loader/dist/es6-module-loader-sans-promises.js',
       './node_modules/es6-module-loader/dist/es6-module-loader-sans-promises.js.map',
       './node_modules/reflect-metadata/Reflect.js',
       './node_modules/reflect-metadata/Reflect.js.map',
       './node_modules/systemjs/dist/system.js',
-      './node_modules/systemjs/dist/system.js.map'
+      './node_modules/systemjs/dist/system.js.map',
+      './node_modules/angular2/node_modules/zone.js/dist/zone.js'
     ]
   }
 };
 
-var builder = new Builder({
+var ng2Builder = new Builder({
   paths: {
     'angular2/*': 'node_modules/angular2/es6/prod/*.es6',
     rx: 'node_modules/angular2/node_modules/rx/dist/rx.js'
@@ -55,9 +64,19 @@ var builder = new Builder({
   }
 });
 
+var appProdBuilder = new Builder({
+  baseURL: 'file:./tmp',
+  meta: {
+    'angular2/angular2': { build: false },
+    'angular2/router': { build: false }
+  }
+});
+
 var tsProject = ts.createProject('tsconfig.json', {
   typescript: require('typescript')
 });
+
+var port = 5555;
 
 // --------------
 // Clean.
@@ -76,20 +95,34 @@ gulp.task('clean.app.dev', function (done) {
        PATH.dest.dev.lib, '!' + join(PATH.dest.dev.lib, '*')], done);
 });
 
+gulp.task('clean.prod', function (done) {
+  del(PATH.dest.prod.all, done);
+});
+
+gulp.task('clean.app.prod', function (done) {
+  // TODO: rework this part.
+  del([join(PATH.dest.prod.all, '**/*'), '!' +
+       PATH.dest.prod.lib, '!' + join(PATH.dest.prod.lib, '*')], done);
+});
+
+gulp.task('clean.tmp', function(done) {
+  del('tmp', done);
+});
+
 // --------------
 // Build dev.
 
 gulp.task('build.ng2.dev', function () {
-  builder.build('angular2/router', join(PATH.dest.dev.lib, 'router.js'), {});
-  return builder.build('angular2/angular2', join(PATH.dest.dev.lib, 'angular2.js'), {});
+  ng2Builder.build('angular2/router', PATH.dest.dev.router, {});
+  return ng2Builder.build('angular2/angular2', PATH.dest.dev.ng2, {});
 });
 
 gulp.task('build.lib.dev', ['build.ng2.dev'], function () {
-  gulp.src(PATH.src.lib)
+  return gulp.src(PATH.src.lib)
     .pipe(gulp.dest(PATH.dest.dev.lib));
 });
 
-gulp.task('build.js.dev', ['clean.app.dev'], function () {
+gulp.task('build.js.dev', function () {
   var result = gulp.src('./app/**/*ts')
     .pipe(plumber())
     .pipe(sourcemaps.init())
@@ -100,9 +133,20 @@ gulp.task('build.js.dev', ['clean.app.dev'], function () {
     .pipe(gulp.dest(PATH.dest.dev.all));
 });
 
-gulp.task('build.app.dev', ['build.js.dev'], function () {
+gulp.task('build.assets.dev', ['build.js.dev'], function () {
   return gulp.src(['./app/**/*.html', './app/**/*.css'])
     .pipe(gulp.dest(PATH.dest.dev.all));
+});
+
+gulp.task('build.index.dev', function() {
+  var target = gulp.src(injectableDevAssetsRef('dev'), { read: false });
+  return gulp.src('./app/index.html')
+    .pipe(inject(target, { transform: transformPath('dev') }))
+    .pipe(gulp.dest(PATH.dest.dev.all));
+});
+
+gulp.task('build.app.dev', function (done) {
+  runSequence('clean.app.dev', 'build.assets.dev', 'build.index.dev', done);
 });
 
 gulp.task('build.dev', function (done) {
@@ -112,13 +156,87 @@ gulp.task('build.dev', function (done) {
 // --------------
 // Build prod.
 
+gulp.task('build.ng2.prod', function () {
+  ng2Builder.build('angular2/router', join('tmp', 'router.js'), {});
+  return ng2Builder.build('angular2/angular2', join('tmp', 'angular2.js'), {});
+});
+
+gulp.task('build.lib.prod', ['build.ng2.prod'], function () {
+  var jsOnly = filter('**/*.js');
+  var lib = gulp.src(PATH.src.lib);
+  var ng2 = gulp.src('tmp/angular2.js');
+  var router = gulp.src('tmp/router.js');
+
+  return series(lib, ng2, router)
+    .pipe(jsOnly)
+    .pipe(sourcemaps.init())
+    .pipe(concat('lib.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(PATH.dest.prod.lib));
+});
+
+gulp.task('build.js.tmp', function () {
+  var result = gulp.src(['./app/**/*ts', '!./app/init.ts'])
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(ts(tsProject));
+
+  return result.js
+    .pipe(gulp.dest('tmp'));
+});
+
+// TODO: add inline source maps (System only generate separate source maps file).
+gulp.task('build.js.prod', ['build.js.tmp'], function() {
+  return appProdBuilder.build('app', join(PATH.dest.prod.all, 'app.js'),
+    { minify: true }).catch(function (e) { console.log(e); });
+});
+
+gulp.task('build.init.prod', function() {
+  var result = gulp.src('./app/init.ts')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(ts(tsProject));
+
+  return result.js
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(PATH.dest.prod.all));
+});
+
+gulp.task('build.assets.prod', ['build.js.prod'], function () {
+  return gulp.src(['./app/**/*.html', './app/**/*.css'])
+    .pipe(gulp.dest(PATH.dest.prod.all));
+});
+
+gulp.task('build.index.prod', function() {
+  var target = gulp.src([join(PATH.dest.prod.lib, 'lib.js'),
+                         join(PATH.dest.prod.all, '**/*.css')], { read: false });
+  return gulp.src('./app/index.html')
+    .pipe(inject(target, { transform: transformPath('prod') }))
+    .pipe(gulp.dest(PATH.dest.prod.all));
+});
+
+gulp.task('build.app.prod', function (done) {
+  // build.init.prod does not work as sub tasks dependencies so placed it here.
+  runSequence('clean.app.prod', 'build.init.prod', 'build.assets.prod',
+              'build.index.prod', 'clean.tmp', done);
+});
+
+gulp.task('build.prod', function (done) {
+  runSequence('clean.prod', 'build.lib.prod', 'clean.tmp', 'build.app.prod',
+              'clean.tmp', done);
+});
+
+// --------------
+// Test.
+
 // To be implemented.
 
 // --------------
 // Serve dev.
 
 gulp.task('serve.dev', ['build.app.dev'], function () {
-  var port = 5555;
   var app;
 
   gulp.watch('./app/**', ['build.app.dev']);
@@ -128,3 +246,36 @@ gulp.task('serve.dev', ['build.app.dev'], function () {
     openResource('http://localhost:' + port);
   });
 });
+
+// --------------
+// Serve prod.
+
+gulp.task('serve.prod', ['build.app.prod'], function () {
+  var app;
+
+  gulp.watch('./app/**', ['build.app.prod']);
+
+  app = connect().use(serveStatic(join(__dirname, PATH.dest.prod.all)));
+  http.createServer(app).listen(port, function () {
+    openResource('http://localhost:' + port);
+  });
+});
+
+// --------------
+// Utils.
+
+function transformPath(env) {
+  return function (filepath) {
+    arguments[0] = filepath.replace('/' + PATH.dest[env].all, '');
+    return inject.transform.apply(inject.transform, arguments);
+  };
+}
+
+function injectableDevAssetsRef() {
+  var src = PATH.src.lib.map(function(path) {
+    return join(PATH.dest.dev.lib, path.split('/').pop());
+  });
+  src.push(PATH.dest.dev.ng2, PATH.dest.dev.router,
+           join(PATH.dest.dev.all, '**/*.css'));
+  return src;
+}
