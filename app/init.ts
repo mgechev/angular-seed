7ed2ce3fd50981dd413a855e03f2c4be435e4af7
mgechev <mgechev@gmail.com>
2015-06-04 16:14:35 +0300

    Update angular version

--- app/init.ts
+++ app/init.ts
@@ -1,4 +1,12 @@
 /// <reference path="../typings/custom.system.d.ts" />
+(function () {
+  var c = System.defined['angular2/core'];
+  var i = c.deps.indexOf('angular2/src/core/compiler/interfaces');
+  if (i >= 0) {
+    c.deps[i] = 'angular2/src/core/zone/ng_zone';
+  }
+}());
+
 System.import('./app')
   .catch(e => console.error(e,
     'Report this error at https://github.com/mgechev/angular2-seed/issues'));
