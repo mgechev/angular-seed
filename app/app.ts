d76f2da5ccab76e719e37444aaeb75fafe820205
ludohenin <ludovic.henin@yahoo.com>
2015-06-08 22:19:07 +0200

    Update d.ts files

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,4 @@
-/// <reference path="../typings/angular2/angular2.d.ts" />
+/// <reference path="../typings/tsd.d.ts" />
 import {Component, View, bootstrap, NgFor} from 'angular2/angular2';
 import {RouteConfig, RouterOutlet, RouterLink, routerInjectables} from 'angular2/router';
 
---
+++ typings/custom.router.d.ts
@@ -0,0 +1,9 @@
+declare module "angular2/router" {
+  var Router: any;
+  var RouterOutlet: any;
+  var RouterLink: any;
+  var RouteParams: any;
+  var routerInjectables: any;
+  var RouteConfigAnnotation: any;
+  var RouteConfig: any;
+}
