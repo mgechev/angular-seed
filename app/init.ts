6ec63dcde27cbf4cbe607cef4eb9bec1489a611c
mgechev <mgechev@gmail.com>
2015-06-18 10:08:50 +0300

    Remove system.js workaround

--- app/init.ts
+++ app/init.ts
@@ -3,14 +3,14 @@ System.config({
   paths: {'*': '*.js?v=<%= VERSION %>'}
 });
 
-// Ugly workaround of systemjs issue https://github.com/systemjs/systemjs/issues/487
-(function () {
-  var c = System.defined['angular2/core'];
-  var i = c.deps.indexOf('angular2/src/core/compiler/interfaces');
-  if (i >= 0) {
-    c.deps[i] = 'angular2/src/core/zone/ng_zone';
-  }
-}());
+//// Ugly workaround of systemjs issue https://github.com/systemjs/systemjs/issues/487
+//(function () {
+//  var c = System.defined['angular2/core'];
+//  var i = c.deps.indexOf('angular2/src/core/compiler/interfaces');
+//  if (i >= 0) {
+//    c.deps[i] = 'angular2/src/core/zone/ng_zone';
+//  }
+//}());
 
 System.import('./app')
   .catch(e => console.error(e,
