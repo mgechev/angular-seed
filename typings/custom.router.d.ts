99336a00d4569d8bd35b12140e272b63371c5f20
ludohenin <ludovic.henin@yahoo.com>
2015-07-08 20:42:07 +0200

    Update typings

--- typings/custom.router.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "angular2/router" {
-  var Router: any;
-  var RouterOutlet: any;
-  var RouterLink: any;
-  var RouteParams: any;
-  var routerInjectables: any;
-  var RouteConfigAnnotation: any;
-  var RouteConfig: any;
-}
