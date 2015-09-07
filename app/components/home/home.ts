d7a34007000ccd7e7c42d945f2734de1af5a905b
ludohenin <ludovic.henin@yahoo.com>
2015-09-07 10:04:19 +0200

    Add router support to unit test

--- app/components/home/home.ts
+++ app/components/home/home.ts
@@ -1,9 +1,11 @@
 import {Component, View} from 'angular2/angular2';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
   selector: 'home'
 })
 @View({
-  templateUrl: './components/home/home.html?v=<%= VERSION %>'
+  templateUrl: './components/home/home.html?v=<%= VERSION %>',
+  directives: [ROUTER_DIRECTIVES]
 })
 export class Home {}
