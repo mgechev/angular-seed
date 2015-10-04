affb521da9bdd598bf1ed44a5e3b967cf3b3e70c
ludohenin <ludovic.henin@yahoo.com>
2015-10-04 10:01:36 +0200

    Fix unit test

--- app/typings/_custom.d.ts
+++ app/typings/_custom.d.ts
@@ -4,6 +4,9 @@
  */
 /// <reference path="./ng2_test.d.ts" />
 
+// to be removed when alpha.38 will be released.
+/// <reference path="../../node_modules/angular2/bundles/typings/angular2/test_lib.d.ts" />
+
 /*
  * tsd generated types
  */
