1f518d1a1f240cccc248bf2d9fda1b3febbc4bbd
ludohenin <ludovic.henin@yahoo.com>
2015-10-14 23:07:11 +0200

    Refactor Typings
    [x] No need to install typings
    [x] Move `app/typings` to `tools`
    [x] Move `tsd_typings` to `tools`
    [ ] TODO: Open issue #xxx about `app/typings.d.ts`

---
+++ app/typings.d.ts
@@ -0,0 +1,4 @@
+// TODO: Fix TS compiler error. Improve tsProjectFn.
+// It should reference definitions from ./tsd_typings directly
+/// <reference path="../tools/typings/tsd/tsd.d.ts" />
+/// <reference path="../tools/typings/ng2_test.d.ts" />
