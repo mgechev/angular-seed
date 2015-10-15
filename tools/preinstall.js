b3122867413b85365b6b52eb332311cb861c4173
tarlepp <tarmo.leppanen@kase.fi>
2015-10-15 21:55:41 +0300

    Fixed preinstall task, which was failing with Travis.

--- tools/preinstall.js
+++ tools/preinstall.js
@@ -4,12 +4,11 @@ var exec = require('child_process').exec;
 
 exec('npm install tsd', installTypings);
 
-
 function installTypings(err, stdout) {
   if (err) return console.log(err);
   console.log(stdout);
 
-  exec('tsd install --clean', function cb(err, stdout) {
+  exec('./node_modules/.bin/tsd install --clean', function cb(err, stdout) {
     if (err) return console.log(err);
     console.log(stdout);
   });
