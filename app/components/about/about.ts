cacf45139fd7c193039834ccfb0d4658633b9a38
Natarajan Ganapathi <natarajanmca11@gmail.com>
2015-10-20 12:50:14 -0700

    Gulp test error fixed.

--- app/components/about/about.ts
+++ app/components/about/about.ts
@@ -8,11 +8,11 @@ import {NameList} from '../../services/name_list';
   directives: [CORE_DIRECTIVES]
 })
 export class AboutCmp {
-  constructor(public list: NameList) {}
-  
-  /**
+  constructor(public list: NameList) {
+  }
+ /*
  * @param newname  any text as input.
- * @returns     return false to prevent default form submit behavior to refresh the page.
+ * @returns return false to prevent default form submit behavior to refresh the page.
  */
   addName(newname): boolean {
     this.list.add(newname.value);
