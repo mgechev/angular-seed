a5626a15a9876be651112ea04907ed4c5fc4b594
Evan Plaice <evanplaice@gmail.com>
2015-11-01 03:27:53 -0800

    Fix #214
    Update /app/bootstrap.tx
    
    Remove deprecated:
    - ROUTER_PRIMARY_COMPONENT - incl in ROUTER_PROVIDERS 90191ce2614ad09
    - bind - deprecated as of angular@2.0.0-alpha.41

--- app/bootstrap.ts
+++ app/bootstrap.ts
@@ -1,8 +1,7 @@
-import {bind, bootstrap} from 'angular2/angular2';
-import {ROUTER_PROVIDERS, ROUTER_PRIMARY_COMPONENT} from 'angular2/router';
+import {bootstrap} from 'angular2/angular2';
+import {ROUTER_PROVIDERS} from 'angular2/router';
 import {AppCmp} from './components/app/app';
 
 bootstrap(AppCmp, [
-  ROUTER_PROVIDERS,
-  bind(ROUTER_PRIMARY_COMPONENT).toValue(AppCmp)
+  ROUTER_PROVIDERS
 ]);
