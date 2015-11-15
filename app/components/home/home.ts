77e928b46b8e0e0f6545e5df9d461142e0dd04a7
UVBrain services <uvbrainsolutions@outlook.com>
2015-11-15 11:45:03 +0530

    Styled about page and home page

--- app/components/home/home.ts
+++ app/components/home/home.ts
@@ -1,8 +1,13 @@
 import {Component} from 'angular2/angular2';
+import {
+ROUTER_DIRECTIVES
+} from 'angular2/router';
 
 @Component({
   selector: 'home',
   templateUrl: './components/home/home.html',
-  styleUrls: ['./components/home/home.css']
+  styleUrls: ['./components/home/home.css'],
+  directives: [ROUTER_DIRECTIVES]
+
 })
-export class HomeCmp {}
+export class HomeCmp { }
