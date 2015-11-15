2bafa21c95d6652637c35610e3ae0622d5d94f64
UVBrain services <uvbrainsolutions@outlook.com>
2015-11-15 11:47:46 +0530

    fixed navigation buttons and their styles

--- app/components/home/home.ts
+++ app/components/home/home.ts
@@ -1,13 +1,8 @@
 import {Component} from 'angular2/angular2';
-import {
-ROUTER_DIRECTIVES
-} from 'angular2/router';
 
 @Component({
   selector: 'home',
   templateUrl: './components/home/home.html',
-  styleUrls: ['./components/home/home.css'],
-  directives: [ROUTER_DIRECTIVES]
-
+  styleUrls: ['./components/home/home.css']
 })
 export class HomeCmp { }
