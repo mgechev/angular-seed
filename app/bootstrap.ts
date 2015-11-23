c103188214379bed068428e120fda98b0fd6730b
mgechev <mgechev@gmail.com>
2015-11-23 12:05:43 +0200

    Update config

--- app/bootstrap.ts
+++ app/bootstrap.ts
@@ -1,7 +1,8 @@
-import {bootstrap} from 'angular2/angular2';
-import {ROUTER_PROVIDERS} from 'angular2/router';
+import {bootstrap, provide} from 'angular2/angular2';
+import {ROUTER_PROVIDERS, APP_BASE_HREF} from 'angular2/router';
 import {AppCmp} from './components/app/app';
 
 bootstrap(AppCmp, [
+  provide(APP_BASE_HREF, { useValue: '<%= APP_BASE + APP_DEST %>' } ),
   ROUTER_PROVIDERS
 ]);
