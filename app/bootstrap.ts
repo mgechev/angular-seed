5362f77d437fbb187b25c2968ac1fcc0aebe253e
mgechev <mgechev@gmail.com>
2015-11-27 14:03:35 +0200

    Change location strategy

--- app/bootstrap.ts
+++ app/bootstrap.ts
@@ -1,8 +1,9 @@
 import {bootstrap, provide} from 'angular2/angular2';
-import {ROUTER_PROVIDERS, APP_BASE_HREF} from 'angular2/router';
+import {ROUTER_PROVIDERS, APP_BASE_HREF, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 import {AppCmp} from './components/app/app';
 
 bootstrap(AppCmp, [
   provide(APP_BASE_HREF, { useValue: '<%= APP_BASE + APP_DEST %>' } ),
-  ROUTER_PROVIDERS
+  ROUTER_PROVIDERS,
+  provide(LocationStrategy, { useClass: HashLocationStrategy })
 ]);
