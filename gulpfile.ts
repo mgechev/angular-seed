381c779c24eb16ba97c5bf4812497344f814c875
mgechev <mgechev@gmail.com>
2015-12-04 19:27:37 +0200

    chore(build): Remove unused task

--- gulpfile.ts
+++ gulpfile.ts
@@ -26,7 +26,6 @@ gulp.task('postinstall', done =>
 gulp.task('build.dev', done =>
   runSequence('clean.dist',
               'tslint',
-              'build.deps',
               'build.sass.dev',
               'build.assets',
               'build.images.dev',
