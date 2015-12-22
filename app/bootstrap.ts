82cf1186ad5f3b081c26eba941e77c517117a780
mgechev <mgechev@gmail.com>
2015-12-22 08:45:33 +0200

    Remove useless provider config

--- app/bootstrap.ts
+++ app/bootstrap.ts
@@ -1,10 +1,9 @@
 import {provide} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
-import {ROUTER_PROVIDERS, APP_BASE_HREF, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 import {AppCmp} from './components/app/app';
 
 bootstrap(AppCmp, [
-  provide(APP_BASE_HREF, { useValue: '<%= APP_ROOT %>' } ),
   ROUTER_PROVIDERS,
   provide(LocationStrategy, { useClass: HashLocationStrategy })
 ]);
