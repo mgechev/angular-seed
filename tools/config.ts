81b4e337714ba69a5f498db2454738cfef9a831f
mgechev <mgechev@gmail.com>
2016-01-30 23:28:50 +0200

    Speedup dev build

--- tools/config.ts
+++ tools/config.ts
@@ -87,10 +87,9 @@ export const PROD_DEPENDENCIES = PROD_NPM_DEPENDENCIES.concat(APP_ASSETS);
 // SystemsJS Configuration.
 const SYSTEM_CONFIG_DEV = {
   defaultJSExtensions: true,
-  paths: {
-    'main': `${APP_ROOT}main`,
-    'hot_loader_main': `${APP_ROOT}hot_loader_main`,
-    '*': `${APP_BASE}node_modules/*`
+  packages: {
+    angular2: { defaultExtension: false },
+    rxjs: { defaultExtension: false }
   }
 };
 
