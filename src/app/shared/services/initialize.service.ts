ae058c97bfd38c870a9824e82d29de861e86bacd
manuel.lang <manuel.lang.311@gmail.com>
2016-02-23 11:44:27 +0100

    adapt changes due to merge from remote-tracking branch

--- src/app/shared/services/initialize.service.ts
+++ src/app/shared/services/initialize.service.ts
@@ -1,9 +1,6 @@
 import {Store} from '../../../store/store';
 import {Injectable} from 'angular2/core';
-import {appInitialized} from '../../../store/actions/app.actions';
 import {UserAuthorizationService} from '../stubs/services/user-authorization.service';
-import {AuthPermissionDto} from '../stubs/dtos/auth-permission-dto';
-import {usersPermissionsLoaded} from '../../../store/actions/session.actions';
 
 @Injectable()
 export class InitializeService {
@@ -15,12 +12,12 @@ export class InitializeService {
   }
 
   private getUsersPermissions():void {
-    var self:InitializeService = this;
+    //var self:InitializeService = this;
 
-    self.userAuthorizationService.getPermissions()
-      .subscribe(function (usersPermissions:Array<AuthPermissionDto>):void {
-        self.store.dispatch(usersPermissionsLoaded(usersPermissions));
-        self.store.dispatch(appInitialized());
-      });
+    /*self.userAuthorizationService.getPermissions()
+     .subscribe(function (usersPermissions:Array<AuthPermissionDto>):void {
+     self.store.dispatch(usersPermissionsLoaded(usersPermissions));
+     self.store.dispatch(appInitialized());
+     });*/
   }
 }
