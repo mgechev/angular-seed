b1265917b3d09c541a1fea7bb391e0a2a736f5f4
manuel.lang <manuel.lang.311@gmail.com>
2016-02-23 11:49:14 +0100

    remove comment

--- src/app/shared/services/initialize.service.ts
+++ src/app/shared/services/initialize.service.ts
@@ -1,6 +1,9 @@
 import {Store} from '../../../store/store';
 import {Injectable} from 'angular2/core';
 import {UserAuthorizationService} from '../stubs/services/user-authorization.service';
+import {AuthPermissionDto} from '../stubs/dtos/auth-permission-dto';
+import {usersPermissionsLoaded} from '../../../store/actions/session.actions';
+import {appInitialized} from '../../../store/actions/app.actions';
 
 @Injectable()
 export class InitializeService {
@@ -12,12 +15,12 @@ export class InitializeService {
   }
 
   private getUsersPermissions():void {
-    //var self:InitializeService = this;
+    var self:InitializeService = this;
 
-    /*self.userAuthorizationService.getPermissions()
-     .subscribe(function (usersPermissions:Array<AuthPermissionDto>):void {
-     self.store.dispatch(usersPermissionsLoaded(usersPermissions));
-     self.store.dispatch(appInitialized());
-     });*/
+    self.userAuthorizationService.getPermissions()
+      .then(function (usersPermissions:Array<AuthPermissionDto>):void {
+        self.store.dispatch(usersPermissionsLoaded(usersPermissions));
+        self.store.dispatch(appInitialized());
+      });
   }
 }
