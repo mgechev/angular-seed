abb67a555fe84464419dfc08321ea761eb570846
ludohenin <ludovic.henin@yahoo.com>
2016-02-26 16:23:06 +0100

    chore(fix): Wrong config in build.asset.prod

--- tools/tasks/build.assets.prod.ts
+++ tools/tasks/build.assets.prod.ts
@@ -1,5 +1,5 @@
 import {join} from 'path';
-import {APP_SRC, APP_DEST, APP_ASSETS} from '../config';
+import {APP_SRC, APP_DEST, ASSETS_SRC} from '../config';
 
 export = function buildAssetsDev(gulp, plugins) {
   // TODO There should be more elegant to prevent empty directories from copying
@@ -19,7 +19,7 @@ export = function buildAssetsDev(gulp, plugins) {
         '!' + join(APP_SRC, '**', '*.ts'),
         '!' + join(APP_SRC, '**', '*.css'),
         '!' + join(APP_SRC, '**', '*.html'),
-        '!' + join(APP_ASSETS, '**', '*.js')
+        '!' + join(ASSETS_SRC, '**', '*.js')
       ])
       .pipe(onlyDirs(es))
       .pipe(gulp.dest(APP_DEST));
