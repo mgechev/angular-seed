42e3ebb77b9c8ec04895b539faeaf88f093f04d5
tsm <attila.egyed.91@gmail.com>
2016-03-01 21:45:18 +0100

    add css-lint task

--- gulpfile.ts
+++ gulpfile.ts
@@ -16,6 +16,7 @@ gulp.task('serve.coverage', task('serve.coverage'));
 gulp.task('build.dev', done =>
   runSequence('clean.dev',
               'tslint',
+              'css-lint',
               'build.assets.dev',
               'build.html_css',
               'build.js.dev',
@@ -44,6 +45,7 @@ gulp.task('build.e2e', done =>
 gulp.task('build.prod', done =>
   runSequence('clean.prod',
               'tslint',
+              'css-lint',
               'build.assets.prod',
               'build.html_css',
               'copy.js.prod',
