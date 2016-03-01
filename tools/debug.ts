c73d8dd39b78d26e62de607e32d38f285930b6dd
ludohenin <ludovic.henin@yahoo.com>
2016-03-01 17:39:43 +0100

    chore: Update debug.ts

--- tools/debug.ts
+++ tools/debug.ts
@@ -5,9 +5,13 @@ require('../gulpfile');
 
 const TASK = argv['task'];
 
-console.log('********************************************************************************');
-console.log('* angular2-seed tools');
-console.log('* task:', TASK);
-console.log('********************************************************************************');
+if (!TASK) {
+  throw new Error('You must specify a task name.');
+}
+
+console.log('**********************');
+console.log('* angular2-seed tools ');
+console.log('* debugging task:', TASK);
+console.log('**********************');
 
 gulp.start(TASK);
