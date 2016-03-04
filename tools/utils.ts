7d3a9acfb9738ad0d89e99cdec8410a5ac006517
Brian Kotek <kotek_brian@bah.com>
2016-03-03 22:12:35 -0500

    Regarding #496, the current tsProjectFn is recreating the TS project each time the utility function is called. Changing it to create the project once and then reuse it on subsequent calls (e.g. after a watch change).

--- tools/utils.ts
+++ tools/utils.ts
@@ -2,9 +2,13 @@ export * from './utils/template_locals';
 export * from './utils/server';
 export * from './utils/tasks_tools';
 
+var _tsProject;
 
 export function tsProjectFn(plugins) {
-  return plugins.typescript.createProject('tsconfig.json', {
-    typescript: require('typescript')
-  });
+  if( !_tsProject ) { 
+    _tsProject = plugins.typescript.createProject('tsconfig.json', {
+      typescript: require('typescript')
+    });
+  }
+  return _tsProject;
 }
