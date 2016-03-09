b24eb770b5a212fde02ca1c174e8b4d5dcd5ac67
Arjen Althoff <a.althoff@payt.nl>
2016-03-09 12:16:14 +0100

    add correct types for error, stdout and stderr

--- tools/tasks/seed/check.versions.ts
+++ tools/tasks/seed/check.versions.ts
@@ -10,7 +10,7 @@ export = () => {
   let semver = require('semver');
 
   exec('npm --version',
-    function (error: string, stdout:string, stderr:string) {
+    function (error: Error, stdout: Buffer, stderr: Buffer) {
       if (error !== null) {
         reportError('npm preinstall error: ' + error + stderr);
       }
@@ -21,7 +21,7 @@ export = () => {
     });
 
   exec('node --version',
-    function (error: string, stdout:string, stderr:string) {
+    function (error: Error, stdout: Buffer, stderr: Buffer) {
       if (error !== null) {
         reportError('npm preinstall error: ' + error + stderr);
       }
