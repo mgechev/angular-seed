9800ad11bf2a1e56770f535a104b5cbffef0290a
d3viant0ne <joshuaw@easymetrics.com>
2016-03-11 20:13:49 -0600

    fix(build): Modify watch task to notify on file change

--- tools/utils/seed/watch.ts
+++ tools/utils/seed/watch.ts
@@ -1,12 +1,14 @@
-import * as gulp from 'gulp';
+import * as runSequence from 'run-sequence';
+import {notifyLiveReload} from '../../utils';
 import * as gulpLoadPlugins from 'gulp-load-plugins';
 import {join} from 'path';
 import {APP_SRC} from '../../config';
 const plugins = <any>gulpLoadPlugins();
 
 export function watch(taskname: string) {
-  return () => {
-    plugins.watch(join(APP_SRC, '**/*'),
-      () => gulp.start(taskname));
+  return function () {
+    plugins.watch(join(APP_SRC, '**'), (e:any) =>
+      runSequence(taskname, () => notifyLiveReload(e))
+    );
   };
 }
