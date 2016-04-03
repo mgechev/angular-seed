5106ac3297178530754babf9f3cf2d96021689cc
Robert Brendler <robert.brendler@iteratec.de>
2016-04-03 19:05:16 +0200

    fix prod build #712

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -131,7 +131,6 @@ export class SeedConfig {
 
   SYSTEM_BUILDER_CONFIG = {
     defaultJSExtensions: true,
-    packageConfigPaths: ['*/package.json'],
     paths: {
       [`${this.TMP_DIR}/*`]: `${this.TMP_DIR}/*`,
       '*': 'node_modules/*'
