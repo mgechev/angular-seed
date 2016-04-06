eee4726bdaf4f3d54ac07142a8e878f1d5fd6c9d
Ross Tang <rt.do.it@gmail.com>
2016-04-06 22:23:36 +0800

    clean up

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,22 @@ import {provide, enableProdMode} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS, APP_BASE_HREF} from 'angular2/router';
 import {AppComponent} from './app/components/app.component';
-// Test to add external dependency
-import {Http, HTTP_PROVIDERS} from 'angular2/http';
-import {AuthHttp, AuthConfig} from 'angular2-jwt';
+// // Test to add external dependency
+// import {Http, HTTP_PROVIDERS} from 'angular2/http';
+// import {AuthHttp, AuthConfig} from 'angular2-jwt';
 // End ot test
 
 if ('<%= ENV %>' === 'prod') { enableProdMode(); }
 
 bootstrap(AppComponent, [
-  ROUTER_PROVIDERS, HTTP_PROVIDERS, // add to test the addition of extenrnal dependency
-  provide(APP_BASE_HREF, { useValue: '<%= APP_BASE %>' }),
-  provide(AuthHttp, { //add to test the addition of external dependency
-    useFactory: (http: Http) => {
-      return new AuthHttp(new AuthConfig(), http);
-    },
-    deps: [Http]
-  }),
+  ROUTER_PROVIDERS, // HTTP_PROVIDERS, // add to test the addition of extenrnal dependency
+  // provide(APP_BASE_HREF, { useValue: '<%= APP_BASE %>' }),
+  // provide(AuthHttp, { //add to test the addition of external dependency
+  //   useFactory: (http: Http) => {
+  //     return new AuthHttp(new AuthConfig(), http);
+  //   },
+  //   deps: [Http]
+  // }),
 ]);
 
 // In order to start the Service Worker located at "./sw.js"
