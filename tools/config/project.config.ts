559de98e9070e095e011bc5c7f3cbbac389479e0
Ross Tang <rt.do.it@gmail.com>
2016-04-06 22:16:55 +0800

    remove angular2-jwt

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -35,7 +35,7 @@ export class ProjectConfig extends SeedConfig {
     ];
 
     // Test to add external depependency
-    (<any>this.SYSTEM_CONFIG_DEV.paths)['angular2-jwt'] =
-        `${this.APP_BASE}node_modules/angular2-jwt/angular2-jwt`;
+    // (<any>this.SYSTEM_CONFIG_DEV.paths)['angular2-jwt'] =
+    //     `${this.APP_BASE}node_modules/angular2-jwt/angular2-jwt`;
   }
 }
