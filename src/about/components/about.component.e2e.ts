52b6710b48d201241cf655facc389a7a3c8d5ca1
Shyam-Chen <Shyam-Chen@users.noreply.github.com>
2016-04-10 20:48:43 +0800

    fix about-e2e (preprotractor ci)

--- src/about/components/about.component.e2e.ts
+++ src/about/components/about.component.e2e.ts
@@ -1,11 +1,12 @@
 describe('About', () => {
-
-  beforeEach( () => {
-    browser.get('about');
+  beforeEach(() => {
+    browser.get('/');
+    browser.waitForAngular();
+    element.all(by.css('nav > a')).get(1).click();
   });
 
   it('should have correct feature heading', () => {
-      expect(element(by.css('sd-app sd-about h2')).getText())
-      .toEqual('Features');
+    let el = element(by.css('sd-app sd-about h2'));
+    expect(el.getText()).toEqual('Features');
   });
 });
