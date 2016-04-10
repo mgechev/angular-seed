a16ca77ad88e397283aa603a89d30fe93625c18e
Shyam-Chen <Shyam-Chen@users.noreply.github.com>
2016-04-10 13:36:39 +0800

    Update app.component.e2e.ts

--- src/app/components/app.component.e2e.ts
+++ src/app/components/app.component.e2e.ts
@@ -1,15 +1,16 @@
 describe('App', () => {
   it('true is true', () => expect(true).toEqual(true));
-/*
+
   beforeEach( () => {
       browser.get('/');
+      browser.waitForAngular();
   });
 
   it('should have a title', () => {
-      // expect(browser.getTitle()).toEqual('My Angular2 App');
-      expect(browser.getLocationAbsUrl()).toMatch('/');
+    expect(browser.getLocationAbsUrl()).toMatch('/');
+    expect(browser.getTitle()).toEqual('My Angular2 App');
   });
-
+/*
   it('should have <nav>', () => {
       expect(element(by.css('sd-app sd-navbar nav')).isPresent()).toEqual(true);
   });
