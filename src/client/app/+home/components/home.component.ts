521f77a98044f903d9af35a41aa7ae4044b6d1a4
Alex Weber <alexweber15@gmail.com>
2016-04-15 11:36:34 -0300

    update directory prefix for template & style urls
    for consistency with: https://github.com/mgechev/angular2-seed/blob/master/src/client/app/%2Babout/components/about.component.ts

--- src/client/app/+home/components/home.component.ts
+++ src/client/app/+home/components/home.component.ts
@@ -5,8 +5,8 @@ import {NameListService} from '../../shared/index';
 
 @Component({
   selector: 'sd-home',
-  templateUrl: 'app/+home/components/home.component.html',
-  styleUrls: ['app/+home/components/home.component.css'],
+  templateUrl: './app/+home/components/home.component.html',
+  styleUrls: ['./app/+home/components/home.component.css'],
   directives: [FORM_DIRECTIVES, CORE_DIRECTIVES]
 })
 export class HomeComponent {
