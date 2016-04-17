6d641372db4d847274997aa9ec613245e8fb7fab
Ben <benpriebe@gmail.com>
2016-04-18 08:32:33 +1000

    Will clean out the dist/dev directory contents

--- tools/tasks/seed/clean.dev.ts
+++ tools/tasks/seed/clean.dev.ts
@@ -1,4 +1,11 @@
-import {DEV_DEST} from '../../config';
-import {clean} from '../../utils';
+import {DEV_DEST} from '../config';
+import {clean} from '../utils';
+import {readdirSync} from 'fs';
+import {join} from 'path';
 
-export = clean(DEV_DEST)
+export = clean(getSubPaths(DEV_DEST))
+
+function getSubPaths(path: string) {
+    let files = readdirSync(path);
+    return files.map((file:string) => join(path, file));
+}
