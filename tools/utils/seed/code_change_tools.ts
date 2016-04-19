537ed53326ebcc288b85c1d9ebbb58b40fc761c2
Craig Bassett <cadriel@users.noreply.github.com>
2016-04-19 02:28:15 -0500

    chore(tools): Expose browserSync configuration (#777)

--- tools/utils/seed/code_change_tools.ts
+++ tools/utils/seed/code_change_tools.ts
@@ -1,27 +1,8 @@
-import {PORT, APP_DEST, APP_BASE, DIST_DIR} from '../../config';
+import {BROWSER_SYNC_CONFIG} from '../../config';
 import * as browserSync from 'browser-sync';
 
 let runServer = () => {
-  let baseDir = APP_DEST;
-  let routes:any = {
-    [`${APP_BASE}${APP_DEST}`]: APP_DEST,
-    [`${APP_BASE}node_modules`]: 'node_modules',
-  };
-
-  if (APP_BASE !== '/') {
-    routes[`${APP_BASE}`] = APP_DEST;
-    baseDir = `${DIST_DIR}/empty/`;
-  }
-
-  browserSync.init({
-    middleware: [require('connect-history-api-fallback')({index: `${APP_BASE}index.html`})],
-    port: PORT,
-    startPath: APP_BASE,
-    server: {
-      baseDir: baseDir,
-      routes: routes
-    }
-  });
+  browserSync.init(BROWSER_SYNC_CONFIG);
 };
 
 let listen = () => {
