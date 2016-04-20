8804ed74dabf7a4b0107546a9d1c1974e6e5b39a
mgechev <mgechev@gmail.com>
2016-04-20 11:30:34 +0300

    chore(progressive): integrate with angular 2 service worker gulp task

--- tools/tasks/seed/build.manifest.ts
+++ tools/tasks/seed/build.manifest.ts
@@ -1,67 +1,21 @@
+import * as gulp from 'gulp';
 import * as fs from 'fs';
 import * as crypto from 'crypto';
 import * as path from 'path';
 import * as walk from 'walk';
 import {APP_DEST, MANIFEST_FILE} from '../../config';
 
-interface Manifest {
-  [key: string]: string;
-}
-
-class DiffingSWManifest {
-  constructor(private basePath: string) {}
-
-  build(files: string[]) {
-    var manifest: Manifest = {};
-    files.filter((file: string) => !file.endsWith(MANIFEST_FILE))
-      .forEach((file) => manifest[file] = this.computeFileHash(file));
-    var manifestPath = path.join(this.basePath, MANIFEST_FILE);
-    fs.writeFileSync(manifestPath, this.generateManifest(manifest));
-  }
-
-  // Compute the hash of the given relative file.
-  computeFileHash(file: string) {
-    var contents = fs.readFileSync(file);
-    return crypto
-      .createHash('sha1')
-      .update(contents)
-      .digest('hex');
-  }
-
-  // Compute the hash of the bundle from the names and hashes of all included files.
-  computeBundleHash(files: string[], manifest: Manifest) {
-    var hash = crypto.createHash('sha1');
-    files.forEach((file: string) => hash.update(manifest[file] + ':' + file));
-    return hash.digest('hex');
-  }
-
-  // Generate the string contents of the manifest.
-  generateManifest(manifest: Manifest) {
-    var files = Object.keys(manifest).sort();
-    var bundleHash = this.computeBundleHash(files, manifest);
-    var contents = files
-      .map((file) => `# sw.file.hash: ${this.computeFileHash(file)}\n${file.replace(this.basePath, '.')}`)
-      .join('\n');
-    return `CACHE MANIFEST
-# sw.bundle: angular2-seed
-# sw.version: ${bundleHash}
-${contents}
-`;
-  }
-}
-
 export = () => {
-  var files: string[] = [];
-
-  var walker  = walk.walk(APP_DEST, { followLinks: false });
-
-  walker.on('file', function(root: string, stat: any, next: Function) {
-    files.push(path.join(root, stat.name));
-    next();
-  });
-
-  walker.on('end', function() {
-    let diffingSWManifest = new DiffingSWManifest(APP_DEST);
-    diffingSWManifest.build(files);
-  });
+  return require('angular2-service-worker')
+    .gulpGenManifest({
+      group: [{
+        name: 'css',
+        sources: gulp.src(`${APP_DEST}/**/*.css`)
+      }, {
+        name: 'js',
+        sources: gulp.src(`${APP_DEST}/**/*.js`)
+      }]
+    })
+    .pipe(gulp.dest(APP_DEST));
 };
+
