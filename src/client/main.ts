f288b9446ff566b9a386865655c04015262eacd4
John Cashmore <cashy@whoooop.co.uk>
2016-04-26 16:16:21 +0100

    fix(APP_BASE_HREF): update reference to APP_BASE_HREF

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,6 +1,7 @@
 import {provide, enableProdMode} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
-import {ROUTER_PROVIDERS, APP_BASE_HREF} from 'angular2/router';
+import {ROUTER_PROVIDERS} from 'angular2/router';
+import {APP_BASE_HREF} from 'angular2/platform/common';
 import {AppComponent} from './app/components/app.component';
 
 if ('<%= ENV %>' === 'prod') { enableProdMode(); }
