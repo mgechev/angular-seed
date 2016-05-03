348440dee0db2570ab14f135e22b34031f107387
mgechev <mgechev@gmail.com>
2016-05-02 20:46:43 -0600

    chore(deps): update to rc.0

--- tools/utils/seed/code_change_tools.ts
+++ tools/utils/seed/code_change_tools.ts
@@ -1,5 +1,6 @@
 import {BROWSER_SYNC_CONFIG} from '../../config';
 import * as browserSync from 'browser-sync';
+import * as path from 'path';
 
 let runServer = () => {
   browserSync.init(BROWSER_SYNC_CONFIG);
@@ -21,11 +22,21 @@ let changed = (files: any) => {
   if (!(files instanceof Array)) {
     files = [files];
   }
+
+  let onlyStylesChanged =
+    files
+      .map((f:string) => path.parse(f).ext)
+      .reduce((prev:string, current:string) => prev && (current === '.scss' || current === '.css'), true);
+
   // if (ENABLE_HOT_LOADING) {
   //   ng2HotLoader.onChange(files);
   // } else {
   //TODO: Figure out why you can't pass a file to reload
-  browserSync.reload(files.path);
+  if (onlyStylesChanged === false) {
+    browserSync.reload(files);
+  }else {
+    browserSync.reload('*.css');
+  }
   //}
 };
 
