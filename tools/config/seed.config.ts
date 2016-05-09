5a251578cadc650b71c8b7b848e779f2b8218508
Nathan Walker <walkerrunpdx@gmail.com>
2016-05-09 09:35:07 -0700

    fix(prod): IE prod build
    System config for production needed `packages`. This also adds `http`
    to better prepare user for potential future use without headache of
    tracking down how to make it work.
    
    closes https://github.com/mgechev/angular2-seed/issues/831

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -106,6 +106,43 @@ export class SeedConfig {
     paths: {
       [`${this.TMP_DIR}/*`]: `${this.TMP_DIR}/*`,
       '*': 'node_modules/*'
+    },
+    packages: {
+      '@angular/core': {
+        main: 'index.js',
+        defaultExtension: 'js'
+      },
+      '@angular/compiler': {
+        main: 'index.js',
+        defaultExtension: 'js'
+      },
+      '@angular/common': {
+        main: 'index.js',
+        defaultExtension: 'js'
+      },
+      '@angular/http': {
+        main: 'index.js',
+        defaultExtension: 'js'
+      },
+      '@angular/platform-browser': {
+        main: 'index.js',
+        defaultExtension: 'js'
+      },
+      '@angular/platform-browser-dynamic': {
+        main: 'index.js',
+        defaultExtension: 'js'
+      },
+      '@angular/router-deprecated': {
+        main: 'index.js',
+        defaultExtension: 'js'
+      },
+      '@angular/router': {
+        main: 'index.js',
+        defaultExtension: 'js'
+      },
+      'rxjs': {
+        defaultExtension: 'js'
+      }
     }
   };
 
