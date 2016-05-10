79e72c744c2fbfd0b8b8bd29112bf727d9cbff59
Shyam-Chen <Shyam-Chen@users.noreply.github.com>
2016-05-10 12:36:15 +0800

    Update home.component.e2e.ts

--- src/client/app/+home/components/home.component.e2e.ts
+++ src/client/app/+home/components/home.component.e2e.ts
@@ -16,8 +16,10 @@ describe('Home', () => {
   it('should add a name to the list using the form', () => {
     element(by.css('sd-app sd-home form input')).sendKeys('Tim Berners-Lee');
     element(by.css('sd-app sd-home form button')).click();
-    let el = element.all(by.repeater('name of nameListService.get()')).get(1);
+    
+    let el = element.all(by.repeater('name of nameListService.get()')).get(0);
     expect(el.getText()).toEqual('Edsger Dijkstra');
+    
     /*
     expect(element(by.css('sd-app sd-home ul')).getText())
       .toEqual('Edsger Dijkstra\nDonald Knuth\nAlan Turing\nGrace Hopper\nTim Berners-Lee');
