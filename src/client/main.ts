360489202f241c928453734d00303aa0c5d2df6f
TheDonDope <idontreallyneedthese@gmail.com>
2016-05-10 21:47:00 +0200

    Reorder module imports
    Reorders the imports of the modules used in the main.ts file.

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,5 +1,5 @@
-import { provide, enableProdMode } from '@angular/core';
 import { APP_BASE_HREF } from '@angular/common';
+import { enableProdMode, provide } from '@angular/core';
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { ROUTER_PROVIDERS } from '@angular/router';
 
