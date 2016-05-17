001c419eb046e1d7866a965420b5358f219d4de6
TheDonDope <idontreallyneedthese@gmail.com>
2016-05-17 22:25:58 +0200

    Apply `moduleId` to @Component annotation
    Sets the `moduleId` property of the @Component annotation to enable
    `templateUrl` and `styleUrls` to be only the file name without the
    surrounding path.

--- src/client/app/+about/about.component.ts
+++ src/client/app/+about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'sd-about',
-  templateUrl: 'app/+about/about.component.html',
-  styleUrls: ['app/+about/about.component.css']
+  templateUrl: 'about.component.html',
+  styleUrls: ['about.component.css']
 })
 /**
  * This class represents the lazy loaded AboutComponent.
--- src/client/app/shared/toolbar/toolbar.component.ts
+++ src/client/app/shared/toolbar/toolbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'sd-toolbar',
-  templateUrl: 'app/shared/toolbar/toolbar.component.html',
-  styleUrls: ['app/shared/toolbar/toolbar.component.css']
+  templateUrl: 'toolbar.component.html',
+  styleUrls: ['toolbar.component.css']
 })
 /**
  * This class represents the toolbar component.
