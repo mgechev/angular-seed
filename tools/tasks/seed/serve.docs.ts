f7c819061c1bd2e2c73a93ff05b7b4c758c82100
Minko Gechev <mgechev@gmail.com>
2016-05-17 10:44:09 +0300

    chore(docs): drop tsdoc (#881)
    Fix #830

--- tools/tasks/seed/serve.docs.ts
+++ tools/tasks/seed/serve.docs.ts
@@ -1,7 +1,8 @@
-import { serveDocs } from '../../utils';
+// import { serveDocs } from '../../utils';
+//
+// /**
+//  * Executes the build process, serving the application documentation using an
+//  * `express` server.
+//  */
+// export = serveDocs;
 
-/**
- * Executes the build process, serving the application documentation using an
- * `express` server.
- */
-export = serveDocs;
