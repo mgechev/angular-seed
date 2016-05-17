6b1a237d5121289b054b9b17d88ef5e0b7a435bb
mgechev <mgechev@gmail.com>
2016-05-17 12:12:33 +0300

    feat(http): add sample http service calls
    Fix #839

--- src/client/app/shared/name-list/name-list.service.ts
+++ src/client/app/shared/name-list/name-list.service.ts
@@ -1,26 +1,50 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/operator/map';
+
 /**
  * This class provides the NameList service with methods to
  * read names and add names.
  */
+@Injectable()
 export class NameListService {
-
   /**
    * The array of initial names provided by the service.
    * @type {Array}
    */
-  names = [
-    'Edsger Dijkstra',
-    'Donald Knuth',
-    'Alan Turing',
-    'Grace Hopper'
-  ];
+  names: string[] = [];
+
+  /**
+   * Contains the currently pending request.
+   * @type {Observable<string[]>}
+   */
+  private request: Observable<string[]>;
+
+  /**
+   * Constructor of the service
+   * @constructor
+   */
+  constructor(private http: Http) {}
 
   /**
    * Returns the array of names.
    * @return {string[]} the array of names
    */
-  get(): string[] {
-    return this.names;
+  get(): Observable<string[]> {
+    if (this.names && this.names.length) {
+      return Observable.from([this.names]);
+    }
+    if (!this.request) {
+      this.request = this.http.get('/assets/data.json')
+        .map((response: Response) => response.json())
+        .map((data: string[]) => {
+          this.request = null;
+          return this.names = data;
+        });
+    }
+    return this.request;
   }
 
   /**
@@ -30,5 +54,5 @@ export class NameListService {
   add(value: string): void {
     this.names.push(value);
   }
-
 }
+
