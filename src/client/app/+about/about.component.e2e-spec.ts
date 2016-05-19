560dec807e1e444274354d154a56c98db4f35e07
Shyam-Chen <Shyam-Chen@users.noreply.github.com>
2016-05-19 17:02:01 +0800

    Indent inconsistent (#899)
    * Indent inconsistent
    
    * Update about.component.e2e-spec.ts
    
    * unnecessary selector
    
    * Update about.component.e2e-spec.ts
    
    * Update home.component.e2e-spec.ts

--- src/client/app/+about/about.component.e2e-spec.ts
+++ src/client/app/+about/about.component.e2e-spec.ts
@@ -1,11 +1,12 @@
 describe('About', () => {
-  beforeEach(() => {
+
+  beforeEach( () => {
     browser.get('/');
     element.all(by.css('nav > a')).get(1).click();
   });
 
   it('should have correct feature heading', () => {
-    let el = element(by.css('sd-about h2'));
-    expect(el.getText()).toEqual('Features');
+    expect(element(by.css('sd-about h2')).getText()).toEqual('Features');
   });
+
 });
