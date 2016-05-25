0bceecdf825b4b646e3e478c08d69cee1a6b4159
Shyam-Chen <Shyam-Chen@users.noreply.github.com>
2016-05-25 22:12:12 +0800

    remove router-deprecated

--- test-main.js
+++ test-main.js
@@ -54,10 +54,6 @@ System.config({
       main: 'index.js',
       defaultExtension: 'js'
     },
-    '@angular/router-deprecated': {
-      main: 'index.js',
-      defaultExtension: 'js'
-    },
     '@angular/router': {
       main: 'index.js',
       defaultExtension: 'js'
