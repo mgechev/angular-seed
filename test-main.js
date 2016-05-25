1eecb8f2ad96e1bf2033a60f6b5e1b974da333a5
Brett Epps <eppsilon@users.noreply.github.com>
2016-05-25 00:11:54 -0500

    chore(tests): remove 'debugger;' statements (#920)

--- test-main.js
+++ test-main.js
@@ -1,4 +1,3 @@
-debugger;
 if (!Object.hasOwnProperty('name')) {
   Object.defineProperty(Function.prototype, 'name', {
     get: function() {
@@ -73,7 +72,6 @@ Promise.all([
   System.import('@angular/core/testing'),
   System.import('@angular/platform-browser-dynamic/testing')
 ]).then(function (providers) {
-  debugger;
   var testing = providers[0];
   var testingBrowser = providers[1];
 
