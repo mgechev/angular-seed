c02777773501de5fd6694e5a92ba5d39c31ff612
陳彥澄 <Shyam-Chen@users.noreply.github.com>
2016-06-16 18:49:15 +0800

    chore(main.ts): use web workers

--- src/client/app/main.ts
+++ src/client/app/main.ts
@@ -1,6 +1,6 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { enableProdMode, provide } from '@angular/core';
-import { bootstrap } from '@angular/platform-browser-dynamic';
+import { bootstrapWorkerApp } from '@angular/platform-browser-dynamic';
 import { ROUTER_PROVIDERS } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -11,7 +11,7 @@ if ('<%= ENV %>' === 'prod') { enableProdMode(); }
  * Bootstraps the application and makes the ROUTER_PROVIDERS and the APP_BASE_HREF available to it.
  * @see https://angular.io/docs/ts/latest/api/platform-browser-dynamic/index/bootstrap-function.html
  */
-bootstrap(AppComponent, [
+bootstrapWorkerApp(AppComponent, [
   ROUTER_PROVIDERS,
   provide(APP_BASE_HREF, { useValue: '<%= APP_BASE %>' })
 ]);
