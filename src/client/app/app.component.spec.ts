c4cb1595d9b6144d40f2c85db254995312e2f784
kiuka <kiuka@kiuka.hu>
2016-06-16 14:51:32 +0200

    Remove FAKE_ROUTER_PROVIDERS from tests, it is unusable

--- src/client/app/app.component.spec.ts
+++ src/client/app/app.component.spec.ts
@@ -2,13 +2,11 @@ import { TestComponentBuilder } from '@angular/compiler/testing';
 import { Component } from '@angular/core';
 import {
   async,
-  beforeEachProviders,
   describe,
   expect,
   inject,
   it
 } from '@angular/core/testing';
-import { ROUTER_FAKE_PROVIDERS } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
 
@@ -17,7 +15,7 @@ export function main() {
   describe('App component', () => {
 
     // Support for testing component that uses Router
-    beforeEachProviders(() => [ROUTER_FAKE_PROVIDERS]);
+    // beforeEachProviders(() => [ROUTER_FAKE_PROVIDERS]);
 
     it('should build without a problem',
        async(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
