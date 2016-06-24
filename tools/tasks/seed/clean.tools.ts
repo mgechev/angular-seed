d527f16a42b9fe35a8f0b75fbcb662028f2f9956
陳彥澄 <Shyam-Chen@users.noreply.github.com>
2016-06-24 08:08:16 -0500

    chore: gulp-util instead of chalk (#1032)
    * Update clean.ts
    
    * Update clean.tools.ts
    
    * Update tasks_tools.ts
    
    * Update typings.json
    
    * Update package.json
    
    * Update check.versions.ts
    
    * Update typings.json
    
    * Update clean.tools.ts
    
    * Update tasks_tools.ts
    
    * Update typings.json
    
    * Update typings.json

--- tools/tasks/seed/clean.tools.ts
+++ tools/tasks/seed/clean.tools.ts
@@ -1,4 +1,3 @@
-import * as chalk from 'chalk';
 import { lstatSync, readdirSync } from 'fs';
 import * as util from 'gulp-util';
 import * as rimraf from 'rimraf';
@@ -36,9 +35,9 @@ function walk(path: any) {
 function deleteAndWalk(path: any) {
   try {
     rimraf.sync(join(path, '*.js'));
-    util.log('Deleted', chalk.yellow(`${path}/*.js`));
+    util.log('Deleted', util.colors.yellow(`${path}/*.js`));
   } catch (e) {
-    util.log('Error while deleting', chalk.yellow(`${path}/*.js`), e);
+    util.log('Error while deleting', util.colors.yellow(`${path}/*.js`), e);
   }
   walk(path);
 }
