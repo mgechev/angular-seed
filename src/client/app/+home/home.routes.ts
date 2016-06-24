e6fd5e65238c95dd3bf8cb79cf573ef9a503efc5
TheDonDope <idontreallyneedthese@gmail.com>
2016-06-24 18:16:09 +0200

    Update Route Configuration for lazy loaded components
    Updates the route configuration for the +about and +home lazy loaded
    component according to the angular guide
    (see: https://angular.io/docs/ts/latest/guide/router.html).
    Furthermore removes the index property which was deprecated with
    @angular/router 3.0.0-alpha.7. Now, the `path: ''` indicates the index
    of a route.

--- src/client/app/+home/home.routes.ts
+++ src/client/app/+home/home.routes.ts
@@ -1,9 +1,10 @@
+import { RouterConfig } from '@angular/router';
+
 import { HomeComponent } from './index';
 
-export const HomeRoutes = [
+export const HomeRoutes: RouterConfig = [
   {
     path: '',
-    component: HomeComponent,
-    index: true
+    component: HomeComponent
   },
 ];
