a0ffe561e6774e28791661ffafbf0aa7b8fe57bc
陳彥澄 <Shyam-Chen@users.noreply.github.com>
2016-06-25 12:39:47 +0800

    gulp instead of gulp-watch

--- tools/utils/seed/watch.ts
+++ tools/utils/seed/watch.ts
@@ -1,24 +1,22 @@
-import * as gulpLoadPlugins from 'gulp-load-plugins';
+import * as gulp from 'gulp';
 import { join } from 'path';
 import * as runSequence from 'run-sequence';
 
 import { APP_SRC, TEMP_FILES } from '../../config';
 import { notifyLiveReload } from '../../utils';
 
-const plugins = <any>gulpLoadPlugins();
-
 /**
  * Watches the task with the given taskname.
  * @param {string} taskname - The name of the task.
  */
 export function watch(taskname: string) {
-  return function () {
-    let paths:string[]=[
-      join(APP_SRC,'**')
-    ].concat(TEMP_FILES.map((p) => { return '!'+p; }));
+  return function() {
+    let paths: string[] = [
+      join(APP_SRC, '**')
+    ].concat(TEMP_FILES.map((p) => { return `!${ p }`; }));
 
-    plugins.watch(paths, (e:any) =>
-      runSequence(taskname, () => notifyLiveReload(e))
+    gulp.watch(paths, (err: any) =>
+      runSequence(taskname, () => notifyLiveReload(err))
     );
   };
 }
