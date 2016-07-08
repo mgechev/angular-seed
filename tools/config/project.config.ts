13d22384d2eedf5e05a00451a42e347afd6043c8
Valentyn Yakymenko <vyakimenko@codeminders.com>
2016-07-08 15:59:55 +0300

    Add APP_ASSETS to project.config.

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -14,11 +14,18 @@ export class ProjectConfig extends SeedConfig {
     super();
     // this.APP_TITLE = 'Put name of your app here';
 
-    // Add third-party libraries to be injected/bundled.
+    // Add `NPM` third-party libraries to be injected/bundled.
     this.NPM_DEPENDENCIES = [
       ...this.NPM_DEPENDENCIES,
       // {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
       // {src: 'lodash/lodash.min.js', inject: 'libs'},
+    ];
+
+    // Add `local` third-party libraries to be injected/bundled.
+    this.APP_ASSETS = [
+      ...this.APP_ASSETS,
+      // {src: `${this.APP_SRC}/your-path-to-lib/libs/jquery-ui.js`, inject: true, vendor: false}
+      // {src: `${this.CSS_SRC}/path-to-lib/test-lib.css`, inject: true, vendor: false},
     ];
 
     /* Add to or override NPM module configurations: */
