f375b3196c8125b4dc483c5fd0418bb121280ce9
shaggyshelar <shaggy.shelar@gmail.com>
2016-07-14 16:36:10 +0530

    added new component for Slot

---
+++ src/client/app/Recruitment Cycle/Shared/Component/InterviewSlot/Component/InterviewSlot.component.ts
@@ -0,0 +1,122 @@
+import {Component, Input, AfterViewInit, OnChanges } from '@angular/core';
+import { ROUTER_DIRECTIVES, Router, OnActivate } from '@angular/router';
+import {CalenderSlot} from '../Model/interviewSlot';
+import {InterviewSlotService } from '../Service/InterviewSlot.service';
+import { ResponseFromAPI, MasterData} from '../../../../../shared/model/common.model';
+import { APIResult} from  '../../../../../shared/constantValue/index';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+
+@Component({
+    moduleId: module.id,
+    selector: 'interview-slot',
+    templateUrl: 'Interviewslot.component.html',
+    directives: [ROUTER_DIRECTIVES],
+    providers: [ToastsManager, InterviewSlotService]
+})
+
+export class InterviewSlotComponent implements OnActivate, AfterViewInit, OnChanges {
+    //@Input() test :string;
+    @Input() RRFID: MasterData = new MasterData();
+    @Input() RRFCode: string;
+    meta: CalenderSlot[] = [];
+    errorMessage: string = '';
+
+
+    constructor(private _router: Router,
+        private _interviewSlotService: InterviewSlotService,
+        public toastr: ToastsManager) {
+        // this.getRRFSlot();
+    }
+
+    routerOnActivate() {
+        //console.write();
+        if (this.meta.length == 0) {
+            this.addNewSlot();
+        }
+    }
+
+    ngAfterViewInit() {
+        // this.getRRFSlot();
+    }
+
+
+    onSaveClick() {
+        var newAddedCalenderSlot: CalenderSlot[] = [];
+        for (var index = 0; index < this.meta.length; index++) {
+            if (this.meta[index].ID == undefined) {
+                newAddedCalenderSlot.push(this.meta[index]);
+            }
+        }
+        if (newAddedCalenderSlot.length > 0) {
+            this._interviewSlotService.SaveCalenderSlot(newAddedCalenderSlot)
+                .subscribe(
+                results => {
+                    if ((<ResponseFromAPI>results).StatusCode === APIResult.Success) {
+                        this.toastr.success((<ResponseFromAPI>results).Message);
+                    } else {
+                        this.toastr.error((<ResponseFromAPI>results).Message);
+                    }
+                },
+                error => this.errorMessage = <any>error);
+        }
+    }
+
+    getRRFSlot() {
+        this._interviewSlotService.getSlotForRRF(this.RRFID)
+            .subscribe(
+            results => {
+                if ((<ResponseFromAPI>results).StatusCode === APIResult.Success) {
+                    this.meta = <any>results;
+                    if (this.meta.length == 0) {
+                        this.addNewSlot();
+                    }
+                } else {
+                    this.toastr.error((<ResponseFromAPI>results).Message);
+                    this.addNewSlot(); //************TODO-delete this
+                }
+            },
+            error => this.errorMessage = <any>error);
+    }
+    onCancelClick() {
+
+    }
+
+    addNewSlot() {
+        var calenderSlot: CalenderSlot = new CalenderSlot();
+        this.meta.push(calenderSlot);
+    }
+    removeSlot(slotTobeRemove: number) {
+        for (var index = 0; index < this.meta.length; index++) {
+            if (this.meta[index].ID == slotTobeRemove) {
+                if (this.meta[index].ID == undefined) {
+                    this.meta.splice(index, 1);
+                } else {
+                    this.removeSlotFromDB(this.meta[index]);
+                    this.meta.splice(slotTobeRemove, 1);
+                }
+            }
+        }
+    }
+
+    removeSlotFromDB(calenderSlot: CalenderSlot) {
+        this._interviewSlotService.deleteCalenderSlot(calenderSlot)
+            .subscribe(
+            results => {
+                if ((<ResponseFromAPI>results).StatusCode === APIResult.Success) {
+                    this.toastr.success((<ResponseFromAPI>results).Message);
+                } else {
+                    this.toastr.error((<ResponseFromAPI>results).Message);
+                }
+            },
+            error => this.errorMessage = <any>error);
+    }
+
+    ngOnChanges(changes: any) {
+        if (changes.RRFCode != undefined && changes.RRFID != undefined) {
+            this.getRRFSlot();
+        }
+
+    }
+
+
+}
---
+++ src/client/app/Recruitment Cycle/Shared/Component/InterviewSlot/Service/InterviewSlot.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import { AuthHttp } from '../../../../../shared/services/authHttp.service';
+import { Config } from '../../../../../shared/config/config';
+import { SpinnerService } from '../../../../../shared/components/spinner/spinner';
+import {CalenderSlot} from '../Model/interviewSlot';
+import { MasterData } from '../../../../../shared/model/common.model';
+
+@Injectable()
+
+export class InterviewSlotService {
+    constructor(private authHttp: AuthHttp,
+        private _spinnerService: SpinnerService) { }
+
+    SaveCalenderSlot(CalenderSlots: CalenderSlot[]) {
+        let url = Config.GetURL('/api/RecruitmentCycle/AddMyRRFInterviewCalendar');
+        this._spinnerService.show();
+        return this.authHttp.post(url, CalenderSlots)
+            .map(this.extractData)
+            .catch(this.handleError)
+            .finally(() => this._spinnerService.hide());
+    }
+
+    deleteCalenderSlot(CalenderSlots: CalenderSlot) {
+        let url = Config.GetURL('/api/RecruitmentCycle/RemoveInterviewerAvailability');
+        this._spinnerService.show();
+        return this.authHttp.post(url, CalenderSlots)
+            .map(this.extractData)
+            .catch(this.handleError)
+            .finally(() => this._spinnerService.hide());
+    }
+
+    getSlotForRRF(RRFID : MasterData){
+         let url = Config.GetURL('/api/RecruitmentCycle/GetMyRRFInterviewCalendar');
+        this._spinnerService.show();
+        return this.authHttp.post(url, { RRFID })
+            .map(this.extractData)
+            .catch(this.handleError)
+            .finally(() => this._spinnerService.hide());
+    }
+
+     private extractData(res: Response) {
+        if (res.status < 200 || res.status >= 300) {
+            throw new Error('Bad response status: ' + res.status);
+        }
+        let body = res.json();
+        return body || {};
+    }
+
+    private handleError(error: Response) {
+        console.log(error);
+        return Observable.throw(error.json().error || 'Server error');
+    }
+}
