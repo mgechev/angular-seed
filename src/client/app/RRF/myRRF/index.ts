57bfbcb20396db7243456f1ce75a52da37059a1c
shaggyshelar <shaggy.shelar@gmail.com>
2016-07-21 19:04:02 +0530

    RRF

--- src/client/app/RRF/myRRF/index.ts
+++ src/client/app/RRF/myRRF/index.ts
@@ -1,4 +1,3 @@
 export * from './components/myRRF.component';
 export * from './components/myRRFAdd.component';
-export * from './components/myRRFList.component';
 export * from './models/rrfDetails';
