3bfc5800b9141e7aa252a14658653cc846a54706
PriyankaShelar10 <pjadhav6@gmail.com>
2016-07-27 18:56:25 +0530

    updated RRFFVIEW page

--- src/client/app/RRF/shared/components/viewRRF/viewRRF.component.ts
+++ src/client/app/RRF/shared/components/viewRRF/viewRRF.component.ts
@@ -1,8 +1,11 @@
-import { Component, Input ,AfterViewInit} from '@angular/core';
+import { Component, Input, AfterViewInit} from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { RRFDetails} from '../../../myRRF/models/rrfDetails';
 import { MyRRFService } from '../../../myRRF/services/myRRF.service';
 import { MasterData} from '../../../../shared/model/common.model';
+import { RRFGridRowComponent } from '../RRFGridRow/RRFGridRow.component';
+import { RRFCandidateListService } from '../../../RRFDashboard/services/RRFCandidatesList.service';
+import {RRFSpecificCandidateList} from '../../../RRFDashboard/model/RRFCandidateList';
 
 
 
@@ -10,17 +13,19 @@ import { MasterData} from '../../../../shared/model/common.model';
     moduleId: module.id,
     selector: 'view-RRF',
     templateUrl: 'viewRRF.component.html',
-    directives: [ROUTER_DIRECTIVES],
+    directives: [ROUTER_DIRECTIVES, RRFGridRowComponent],
     styleUrls: ['viewRRF.component.css'],
-    providers: [MyRRFService]
+    providers: [MyRRFService, RRFCandidateListService]
 })
 
 
 export class ViewRRFComponent implements AfterViewInit {
     @Input() RRFID: MasterData = new MasterData();
     selectedRRF: RRFDetails = new RRFDetails();
+    candidatelist: RRFSpecificCandidateList[] = [];
     errorMessage: string = '';
-    constructor(private _myRRFService: MyRRFService) {
+    constructor(private _myRRFService: MyRRFService,
+        private _RRFCandidateService: RRFCandidateListService) {
     }
 
     getRRFDetails(rrfID: MasterData) {
@@ -32,8 +37,31 @@ export class ViewRRFComponent implements AfterViewInit {
             error => this.errorMessage = <any>error);
     }
 
-    ngAfterViewInit(){
+    getCandidatesForRRF(rrfID: MasterData) {
+        this._RRFCandidateService.getCandidatesForRRF(rrfID.Value)
+            .subscribe(
+            (results: RRFSpecificCandidateList[]) => {
+                this.candidatelist = results;
+                this.setValue();
+            },
+            error => this.errorMessage = <any>error);
+    }
+
+    setValue() {
+        for (var index = 0; index < this.candidatelist.length; index++) {
+            if (this.candidatelist[index].InterviewDetails.Status !== null) {
+                //
+            } else {
+                this.candidatelist[index].InterviewDetails.Status = 'Not Scheduled';
+                if (this.candidatelist[index].InterviewDetails.Round.Value === null)
+                    this.candidatelist[index].InterviewDetails.Round.Value = '--';
+            }
+        }
+    }
+
+    ngAfterViewInit() {
         this.getRRFDetails(this.RRFID);
+        this.getCandidatesForRRF(this.RRFID);
     }
 
 
