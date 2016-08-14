e8e279f8bbc9854cfb064208ec9d30a2a18573cd
mgechev <mgechev@gmail.com>
2016-08-13 17:43:17 -0700

    feat: showcase of how to have multiple apps with shared codebase

--- src/client/foo/app.component.ts
+++ src/client/foo/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
-import { Config, NavbarComponent, ToolbarComponent } from './../shared/index';
+import { Config, NameListService } from '../shared/index';
 
 /**
  * This class represents the main application component. Within the @Routes annotation is the configuration of the
@@ -10,11 +10,19 @@ import { Config, NavbarComponent, ToolbarComponent } from './../shared/index';
   moduleId: module.id,
   selector: 'sd-app',
   templateUrl: 'app.component.html',
-  directives: [ROUTER_DIRECTIVES, NavbarComponent, ToolbarComponent]
+  directives: [ROUTER_DIRECTIVES]
 })
 
 export class AppComponent {
-  constructor() {
+
+  names: string[];
+
+  constructor(private nameListService: NameListService) {
     console.log('Environment config', Config);
+  }
+
+  ngOnInit() {
+    this.nameListService.get()
+      .subscribe((names: string[]) => this.names = names);
   }
 }
--- src/client/foo/app.module.ts
+++ src/client/foo/app.module.ts
@@ -4,14 +4,11 @@ import { APP_BASE_HREF } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { routes } from './app.routes';
 
-import { AboutModule } from './+about/about.module';
-import { HomeModule } from './+home/home.module';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [BrowserModule, HttpModule, RouterModule.forRoot(routes), AboutModule, HomeModule, SharedModule.forRoot()],
+  imports: [BrowserModule, HttpModule, SharedModule.forRoot()],
   declarations: [AppComponent],
   providers: [{
     provide: APP_BASE_HREF,
