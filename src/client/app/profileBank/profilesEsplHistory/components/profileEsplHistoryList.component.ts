0db2c9754ad37f65c7dc8da6d91c12b388e37a71
shailesh-sangekar <shailesh.sangekar@eternussolutions.com>
2016-08-18 15:39:33 +0530

    Integrated API for Candidates ESPL history; fixes for actions alignment;

--- src/client/app/profileBank/profilesEsplHistory/components/profileEsplHistoryList.component.ts
+++ src/client/app/profileBank/profilesEsplHistory/components/profileEsplHistoryList.component.ts
@@ -3,7 +3,7 @@ import { ROUTER_DIRECTIVES, OnActivate, Router } from '@angular/router';
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 import {IfAuthorizeDirective} from '../../../shared/directives/ifAuthorize.directive';
 import { ProfileBankPipe }from '../../shared/filter/profileBank.pipe';
-import { ProfileInterviewHistory }from '../models/profileEsplHistoryModel';
+import { ProfileInterviewHistory, AllInterviewsHistory }from '../models/profileEsplHistoryModel';
 import { MasterData } from  '../../../shared/model/index';
 import { ProfileEsplHistoryService } from '../services/profileEsplHistory.service';
 
@@ -18,11 +18,12 @@ import { ProfileEsplHistoryService } from '../services/profileEsplHistory.servic
 })
 
 export class ProfileEsplHistoryListComponent implements OnActivate {
+    allInterviewsHistory: AllInterviewsHistory = new AllInterviewsHistory();
     profileHistory: ProfileInterviewHistory;
     profileHistoryCollection: Array<ProfileInterviewHistory> = new Array<ProfileInterviewHistory>();
     historyOfCandidate: MasterData;
     errorMessage: string;
-
+    NORECORDSFOUND: boolean = false;
     constructor(private _router: Router,
         public toastr: ToastsManager,
         private profilesHistoryService: ProfileEsplHistoryService) {
@@ -41,14 +42,32 @@ export class ProfileEsplHistoryListComponent implements OnActivate {
         this.profilesHistoryService.getProfilesInterviewHistory(_candidateID)
             .subscribe(
             (results: any) => {
-                if (results.Profiles !== undefined && results.Profiles.length > 0) {
+                //if (results.Profiles !== undefined && results.Profiles.length > 0) {
+                if (results !== undefined && results.length > 0) {
                     this.profileHistoryCollection = <any>results;
                 } else {
                     this.toastr.error('No Records Found.');
+                    this.profileHistoryCollection = null;
                 }
             },
             error => this.errorMessage = <any>error);
     }
+
+    // getProfilesAllHistory() {
+    //     try {
+    //         this.profilesHistoryService.getProfilesInterviewHistory(this.allInterviewsHistory.GrdOperations)
+    //             .subscribe(
+    //             (results: any) => {
+    //                 if (results.Profiles !== undefined && results.Profiles.length > 0) {
+    //                     this.allInterviewsHistory = <AllInterviewsHistory>results;
+    //                 } else { this.NORECORDSFOUND = true; }
+    //             },
+    //             error => this.errorMessage = <any>error);
+    //     } catch (error) {
+    //         this.allInterviewsHistory = new AllInterviewsHistory();
+    //     }
+    // }
+
     /**Get data from session */
     getSessionOf<T>(variableName: string): T {
         var _requestedIef = sessionStorage.getItem(variableName);
