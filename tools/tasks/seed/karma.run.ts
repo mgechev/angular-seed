eece3e1a11e2fcc744beb896870a8236d84c23ca
mgechev <mgechev@gmail.com>
2016-08-20 18:55:16 -0700

    feat(build): improve testing

---
+++ tools/tasks/seed/karma.run.ts
@@ -0,0 +1,7 @@
+import { startKarma } from '../../utils/seed/karma.start';
+/**
+ * Executes the build process, running all unit tests using `karma`.
+ */
+export = (done: any) => {
+  return startKarma(done);
+};
