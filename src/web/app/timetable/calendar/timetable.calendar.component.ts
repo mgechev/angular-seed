d7f192ad90b7336d8b4e4a16503c0401ff0565b7
ShanonJackson <phazeblade@hotmail.com>
2016-09-13 23:52:37 +1200

    transferring changes to desktop

---
+++ src/web/app/timetable/calendar/timetable.calendar.component.ts
@@ -0,0 +1,16 @@
+import { Component} from '@angular/core';
+import { Router } from '@angular/router';
+import {ROUTER_DIRECTIVES } from '@angular/router';
+
+@Component({
+  moduleId: module.id,
+  selector: 'timetable-calendar',
+  templateUrl: 'timetable.calendar.component.html',
+  directives: []
+})
+
+export class CalendarComponent {
+  example:string = "yanan";
+
+
+}
