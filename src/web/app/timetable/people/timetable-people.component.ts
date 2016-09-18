1b99a522b2a36aa2f60ba413f54bb5fda4f2bd41
PyroPrav <veal007@hotmail.com>
2016-09-18 17:13:47 +1200

    people component confirmation icons working

--- src/web/app/timetable/people/timetable-people.component.ts
+++ src/web/app/timetable/people/timetable-people.component.ts
@@ -2,6 +2,23 @@ import { Component} from '@angular/core';
 import { Router } from '@angular/router';
 import {ROUTER_DIRECTIVES } from '@angular/router';
 
+export class data{
+  id: number;
+  email: string;
+  completed: boolean;
+};
+
+
+  //TEST DATA SET FOR PEOPLE
+const datas: data[] = [
+  {id: 1, email: "phazeblade@hotmail.com", completed:false},
+  {id: 2, email: "Patrick@no.this.is.patrick.com", completed:true},
+  {id: 3, email: "praveal.lal@live.com", completed:true},
+  {id: 4, email: "shoar@ShoarAgainstWar.com", completed:true}
+];
+
+
+
 @Component({
   moduleId: module.id,
   selector: 'timetable-people',
@@ -11,6 +28,11 @@ import {ROUTER_DIRECTIVES } from '@angular/router';
 })
 
 export class PeopleComponent {
-  emails: string[] = ["phazeblade@hotmail.com", "Patrick@no.this.is.patrick.com", "Praveal.lal@live.com", "Shoar@ShoarAgainstWar.com"]
-  
+  dataOne = datas
+
+  //emails: any = [["phazeblade@hotmail.com"], ["Patrick@no.this.is.patrick.com"], ["Praveal.lal@live.com"], ["Shoar@ShoarAgainstWar.com"]]
+
+  private isCompleted = (completed:boolean) =>  {
+    return completed;
+  };
 }
