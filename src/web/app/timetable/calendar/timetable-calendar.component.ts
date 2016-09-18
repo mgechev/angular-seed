ef9d50df49b5c3c8eda8677e43b5cae0bb80c568
Shoar <Shoar@shoar.org>
2016-09-18 17:25:10 +1200

    Calendar selection and array data

--- src/web/app/timetable/calendar/timetable-calendar.component.ts
+++ src/web/app/timetable/calendar/timetable-calendar.component.ts
@@ -12,6 +12,27 @@ import {ROUTER_DIRECTIVES } from '@angular/router';
 
 export class CalendarComponent {
   example:string = "yanan";
+  numbers: any = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]
+  dateNames: any = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  dates: string[] = []
 
+  private dateSelect = (date: String) => {
+    if (this.dates.includes(date)) {
+      this.dates.splice(this.dates.indexOf(date), 1)
+      console.log(this.dates);
+    }
+    else {
+      this.dates.push(date)
+      console.log(this.dates);
+    }
+  }
 
+  private dateSelected = (date: String) => {
+    if (this.dates.includes(date)){
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 }
