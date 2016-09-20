df1b1b2687d734436c44967db7cc69433bcdc692
Gauntly <gauntly@protonmail.com>
2016-09-20 16:12:15 +1200

    Schedule works in debugger. Adds and Splices properly. - Need to implement function to display color when time is selected.

--- src/web/app/timetable/schedule/timetable-schedule.component.ts
+++ src/web/app/timetable/schedule/timetable-schedule.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import {ROUTER_DIRECTIVES } from '@angular/router';
 import {Pipe, PipeTransform} from '@angular/core';
 import any = jasmine.any;
+import {timeInterval} from "rxjs/operator/timeInterval";
 /*
 Stores one date with selected times for one email/'person'.
 
@@ -27,95 +28,59 @@ export class dateTimes{
 
 export class ScheduleComponent implements AfterViewInit {
 
-  dates:any = ["10/10/2015", "1/04/2016", "2/09/2016", "20/09/2016", "69/69/69","69/69/69","69/69/69"]
+  dates:any = ["10/10/2015", "1/04/2016", "2/09/2016", "20/09/2016", "01/01/2001","01/01/2001","01/01/2001"]
   minTimeMaxTime:any = ["9:00","18:00"];
   availableTimes:string[] = ["09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00"]
   timeBlock:string[] =[]
-  fakeSelectedDateTimes: selectedDateTimes = {date:"",listTimes:[]}
-  personalData: dateTimes = {email:"Fake Email",availableTimes:[this.fakeSelectedDateTimes]}
-  storedTimes:string[] = []
-  happened:boolean = false
+  personalData: dateTimes = {email:"Fake Email",availableTimes:[]}
+  trueFlag:boolean = false
 
   private clickCell = (date:string, time:string) => {
 
-    //The ugliest line of code in the world
+    this.personalData.availableTimes.forEach(x => {
+      this.trueFlag = false
 
-    for (var i = 0; i < this.personalData.availableTimes.length; i++) {
+      this.personalData.availableTimes.forEach(z => {
+        if(z.date === date) this.trueFlag = true
+        else this.trueFlag = false
+      })
 
-      this.happened = false
-      this.storedTimes = []
-      var doesContain = this.personalData.availableTimes.filter(x => this.personalData.availableTimes[i].date === date);
-      if (doesContain.length > 0) {
-        console.log("Occured1")
-        if (this.storedTimes.includes(time)) {
-          this.storedTimes.splice(this.storedTimes.indexOf(time), 1)
+
+      if(this.trueFlag) {
+        if(x.listTimes.includes(time) && (x.date === date)) {
+          x.listTimes.splice(x.listTimes.indexOf(time),1)
         } else {
-          this.storedTimes.push(time)
+          x.listTimes.push(time)
         }
-      }else{
-        console.log("Occured2")
-        this.happened = true
-        // this.personalData.availableTimes[i].date = date
-
-        if (this.storedTimes.includes(time)) {
-
-          this.storedTimes.splice(this.storedTimes.indexOf(time), 1)
-        } else {
-          this.storedTimes.push(time)
-        }
+      } else{
+        this.personalData.availableTimes.push({date:date,listTimes:[time]})
       }
-      // console.log(this.storedTimes)
-      if(this.happened) {
-        this.personalData.availableTimes.push({date:date,listTimes:this.storedTimes})
-        console.log(this.personalData.availableTimes)
-      }else{
-        this.personalData.availableTimes[i].listTimes = this.storedTimes
       }
+    );
+    if(this.personalData.availableTimes.length == 0) {
+      this.personalData.availableTimes.push({date:date,listTimes:[time]})
     }
-    console.log(this.personalData.availableTimes)
-
-
 
   }
-  private isSelected = (time:string) =>{
-    return this.personalData.availableTimes.includes(time)
-    // if(this.personalData.availableTimes.includes(time)){
-    //
-    // }
+
+
+  private isSelected = (date:string, time:string) =>{
+    // this.personalData.availableTimes.find(, function(o) {return o.date === date})
+    this.personalData.availableTimes.forEach(x =>{
+      if(x.listTimes.includes(time) && (x.date === date)) {
+        console.log("Called selected")
+        console.log()
+        return true
+      }
+      })
+    //console.log("Called deselect")
+    return false
+
   }
 
-  // arraySplit = (dateCan: String) =>{
-  //   while(this.timesBlock.length > 0){
-  //     console.log(this.timesBlock);
-  //     this.timesBlock.pop();
-  //   }
-  //
-  // }
+
   ngAfterViewInit(){
   }
-
-
-
-
-
-  // exampleMethod = () =>{
-  //   timeBlock:String[""] = this.timesBlock
-  //   while(!{
-  //
-  //
-  //   }
-  // }
-  /* example:string = "yanan";
-   //Day/Month/Year
-   */
-
-
-  /*
-   Take in times array.
-   Create new array based on the times array.
-   Push the new array to time module left of the selected dates module.
-   */
-
 
 }
 
