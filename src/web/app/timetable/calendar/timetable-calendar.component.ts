343a164385af307d57fbeebe84345ab767b30412
Shoar <Shoar@shoar.org>
2016-09-21 12:29:15 +1200

    -Removed unused CSS in Calendar file -Removed unused HTML code in Calendar file +Added currentMonthString to represent month name +Added function to get days in a month +Added function to display date respresent in (row,col) +Broken function to get start date (in name, e.g. Monday) ~Changed currentMonth to represent the number of the month instead

--- src/web/app/timetable/calendar/timetable-calendar.component.ts
+++ src/web/app/timetable/calendar/timetable-calendar.component.ts
@@ -13,14 +13,16 @@ import {ROUTER_DIRECTIVES } from '@angular/router';
 export class CalendarComponent {
   example:string = "yanan";
   months:any= ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-  numbers: any = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]
+  days: any = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
   dateNames: any = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
   dates: string[] = []
+  write:any = 0
 
   // Current day/month/day
   date:any = new Date()
   currentDay:string = this.date.getDate()
-  currentMonth:string = this.months[this.date.getMonth()]
+  currentMonth:string = this.date.getMonth()
+  currentMonthString:string = this.months[this.date.getMonth()]
   currentYear:any = this.date.getFullYear()
 
 
@@ -44,27 +46,29 @@ export class CalendarComponent {
   }
 
   private nextMonth = () => {
-    var current = this.months.indexOf(this.currentMonth)
+    var current = parseInt(this.currentMonth)
     current++
     if (current > 11) {
       current = 0
       this.nextYear()
     }
-    this.currentMonth = (this.months[current])
-    console.log(current)
-    console.log(this.months[current])
+    this.currentMonthString = (this.months[current])
+    this.currentMonth = current.toString()
+    //console.log(current)
+    //console.log(this.months[current])
   }
 
   private prevMonth = () => {
-    var current = this.months.indexOf(this.currentMonth)
+    var current =  parseInt(this.currentMonth)
     current--
     if (current < 0) {
       current = 11
       this.prevYear()
     }
-    this.currentMonth = (this.months[current])
-    console.log(current)
-    console.log(this.months[current])
+    this.currentMonthString = (this.months[current])
+    this.currentMonth = current.toString()
+    //console.log(current)
+    //console.log(this.months[current])
   }
 
   private nextYear = () => {
@@ -75,7 +79,48 @@ export class CalendarComponent {
     this.currentYear--
   }
 
-  private updateDays = () => {
+  private getDays = (month:any) => {
+    var days = 0
+    switch (parseInt(month)) { // Yes this code is extremely ugly but I can't do it in one line because javascript sucks
+      case 0:
+      case 2:
+      case 4:
+      case 6:
+      case 7:
+      case 9:
+      case 11: // 31 days months
+            days = 31
+            break
+      case 3:
+      case 5:
+      case 8:
+      case 10: // 30 days months
+            days = 30
+            break
+      case 1: // Feb
+            if ((parseInt(this.currentYear)%4 == 0) && (parseInt(this.currentYear)%100 != 0) || (parseInt(this.currentYear)%400 == 0)){
+              days = 29
+            }else {
+              days = 28
+            }
+            break
+    }
+    return days;
 
   }
+
+  private getStartDay = (day:any, month:any, year:any) => {
+    var startDay = new Date(day, month, year)
+    return startDay
+  }
+
+  private displayDay = (row:any, col:any) => {
+    //console.log(this.write)
+    this.write++
+    var currentDay = this.days[(row-1)+((col-1)*7)];
+    if (currentDay <= this.getDays(this.currentMonth))
+      return currentDay
+    else
+      return (((row)+((col-1)*7)) - this.getDays(this.currentMonth))
+}
 }
