e9f3786557bcdf8f2b9e0716a723fefe3ee31201
Gauntly <gauntly@protonmail.com>
2016-09-21 15:09:51 +1200

    Removed unused trait afterViewInit.

--- src/web/app/timetable/schedule/timetable-schedule.component.ts
+++ src/web/app/timetable/schedule/timetable-schedule.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, AfterViewInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import {ROUTER_DIRECTIVES } from '@angular/router';
 import any = jasmine.any;
