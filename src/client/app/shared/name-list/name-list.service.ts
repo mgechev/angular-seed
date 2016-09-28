69a94d6f14c940ce0bdeb9f50eecd48a823470b2
karlhaas <haaskarl81@gmail.com>
2016-09-28 12:13:42 +0200

    Add all needed rxjs/ngrx operators to one central file (#1397)
    Import rxjs/ngrx operators in a separate file

--- src/client/app/shared/name-list/name-list.service.ts
+++ src/client/app/shared/name-list/name-list.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
 
 /**
  * This class provides the NameList service with methods to read names and add names.
