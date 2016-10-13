7c694e0197fae9ed5f6e6f6e056e25fd805e6da4
Ryan Pendergast <ryanp@doapps.com>
2016-10-13 15:28:11 -0500

    only need throw

--- src/client/app/shared/name-list/name-list.service.ts
+++ src/client/app/shared/name-list/name-list.service.ts
@@ -3,9 +3,7 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/observable/throw';
-import 'rxjs/add/operator/map';
 // import 'rxjs/add/operator/do';  // for debugging
-import 'rxjs/add/operator/catch';
 
 /**
  * This class provides the NameList service with methods to read names and add names.
