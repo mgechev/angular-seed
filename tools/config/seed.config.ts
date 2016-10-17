b972f1221c7340980d3da9eae548a33a71e2ea08
Brian Kotek <kotek_brian@bah.com>
2016-10-17 10:48:02 -0400

    Fix #1475 by removing packageConfigPaths from seed config.

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -342,11 +342,6 @@ export class SeedConfig {
    */
   SYSTEM_CONFIG_DEV: any = {
     defaultJSExtensions: true,
-    packageConfigPaths: [
-      `/node_modules/*/package.json`,
-      `/node_modules/**/package.json`,
-      `/node_modules/@angular/*/package.json`
-    ],
     paths: {
       [this.BOOTSTRAP_MODULE]: `${this.APP_BASE}${this.BOOTSTRAP_MODULE}`,
       '@angular/common': 'node_modules/@angular/common/bundles/common.umd.js',
