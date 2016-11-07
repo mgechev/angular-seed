3f33d41e63c3b2925d42bfea9c8e51cda15c2edd
ruffiem <ruffiem@gmail.com>
2016-11-07 19:23:08 +0100

    (feat) reduced complexity when adding n additional packages (#1593)
    * (feat) reduced complexity when adding n additional packages
    
    * (feat) changed class instance property to local variable
    
    * (bug) fixed ExtendPackages interface implementation
    
    * (feat) commenting additional packages example
    
    * (bug) commenting ExtendPackages unused import

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -1,6 +1,7 @@
 import { join } from 'path';
 
 import { SeedConfig } from './seed.config';
+// import { ExtendPackages } from './seed.config.interfaces';
 
 /**
  * This class extends the basic seed configuration, allowing for project specific overrides. A few examples can be found
@@ -30,6 +31,31 @@ export class ProjectConfig extends SeedConfig {
       // {src: `${this.APP_SRC}/your-path-to-lib/libs/jquery-ui.js`, inject: true, vendor: false}
       // {src: `${this.CSS_SRC}/path-to-lib/test-lib.css`, inject: true, vendor: false},
     ];
+
+    // Add packages (e.g. lodash)
+    // let additionalPackages: ExtendPackages[] = [{
+    //   name: 'lodash',
+    //   path: `${this.APP_BASE}node_modules/lodash/lodash.js`,
+    //   packageMeta: {
+    //     main: 'index.js',
+    //     defaultExtension: 'js'
+    //   }
+    // }];
+    //
+    // or
+    //
+    // let additionalPackages: ExtendPackages[] = [];
+    //
+    // additionalPackages.push({
+    //   name: 'lodash',
+    //   path: `${this.APP_BASE}node_modules/lodash/lodash.js`,
+    //   packageMeta: {
+    //     main: 'index.js',
+    //     defaultExtension: 'js'
+    //   }
+    // });
+    //
+    // this.addPackagesBundles(additionalPackages);
 
     /* Add to or override NPM module configurations: */
     // this.mergeObject(this.PLUGIN_CONFIGS['browser-sync'], { ghostMode: false });
