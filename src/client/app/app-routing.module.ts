890e91cd865751204cc998f34aa71d770a78641d
Douglas Boleta <douglas.boleta@elotech.com.br>
2016-12-13 09:08:31 -0200

    included bootstrap_dir on system_builder_config

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forRoot([
-       { path: 'lazy-about', loadChildren: 'app/+about/lazy-about.module.js#LazyAboutModule' }
+       { path: 'lazy-about', loadChildren: 'app/+about/lazy-about.module#LazyAboutModule' }
     ])
   ],
   exports: [RouterModule]
