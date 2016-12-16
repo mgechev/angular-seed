ad9bb8bac90672400528d9f62c025094efea9f95
Douglas Boleta <douglas.boleta@elotech.com.br>
2016-12-16 08:53:48 -0200

    used service from main in a lazy module

--- src/client/app/lazy-about/lazy-about.component.ts
+++ src/client/app/lazy-about/lazy-about.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { NameListService } from '../shared/index';
 /**
  * This class represents the lazy loaded AboutComponent.
  */
@@ -9,7 +9,17 @@ import { Component } from '@angular/core';
   templateUrl: 'lazy-about.component.html',
   styleUrls: ['lazy-about.component.css']
 })
-export class LazyAboutComponent  {
+export class LazyAboutComponent implements OnInit {
+
+  names: any[] = [];
+
+  constructor(public nameListService: NameListService) {}
+
+  ngOnInit() {
+    this.nameListService.get()
+      .subscribe(names => this.names = names );
+  }
+
 
 
 }
