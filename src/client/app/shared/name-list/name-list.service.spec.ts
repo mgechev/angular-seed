92342a557eda25dc62daf25f352b62e822b4270a
Jean-Philippe Quéméner <JohnnyQQQQ@users.noreply.github.com>
2016-12-17 11:49:22 +0100

    fix: remove brace after semicolon

--- src/client/app/shared/name-list/name-list.service.spec.ts
+++ src/client/app/shared/name-list/name-list.service.spec.ts
@@ -1,5 +1,5 @@
 import { BaseRequestOptions, ConnectionBackend, Http, Response, ResponseOptions } from '@angular/http';
-import { TestBed, async } from '@angular/core/testing';}
+import { TestBed, async } from '@angular/core/testing';
 import { MockBackend } from '@angular/http/testing';
 
 import { Observable } from 'rxjs/Observable';
