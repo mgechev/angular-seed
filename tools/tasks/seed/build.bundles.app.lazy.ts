89a842a6e3180739ded7be0d02ad7646f136b072
Douglas Boleta <douglas.boleta@elotech.com.br>
2016-12-19 17:34:06 -0200

    included support for lazy loading on aot

--- tools/tasks/seed/build.bundles.app.lazy.ts
+++ tools/tasks/seed/build.bundles.app.lazy.ts
@@ -3,6 +3,8 @@ import * as gulp from 'gulp';
 import * as gulpLoadPlugins from 'gulp-load-plugins';
 import * as Builder from 'systemjs-builder';
 import * as util from 'gulp-util';
+import * as path from 'path';
+import { existsSync } from 'fs';
 
 import Config from '../../config';
 
@@ -22,14 +24,21 @@ const slash = require('slash');
 export = (done: any) => {
 
   let builder = new Builder(Config.SYSTEM_BUILDER_CONFIG);
-  let mainBundle = `${Config.TMP_DIR}/${Config.BOOTSTRAP_PROD_MODULE}`;
+  let mainBundle = '';
 
   return gulp.src(Config.LAZY_MATCH_EXPRESSION)
     .pipe(plugins.flatmap((stream: any, file: any) => {
-      let fileRelative: string = slash(join(Config.BOOTSTRAP_DIR, file.relative));
+      const ext = path.extname(file.relative);
+      let fileRelative: string = slash(join(Config.BOOTSTRAP_DIR, file.relative.replace(ext, '')));
+      if (existsSync(file.path.replace(ext,'.ngfactory.js'))) {
+        mainBundle = `${Config.TMP_DIR}/${Config.BOOTSTRAP_FACTORY_PROD_MODULE}`;
+        fileRelative+='.ngfactory';
+      } else {
+        mainBundle = `${Config.TMP_DIR}/${Config.BOOTSTRAP_PROD_MODULE}`;
+      }
       let lazyBundle = `${slash(join(Config.TMP_DIR, fileRelative))} - ${mainBundle}`;
       util.log('Building lazy bundle', lazyBundle);
-      builder.buildStatic(lazyBundle, join(Config.APP_DEST, fileRelative),
+      builder.buildStatic(lazyBundle, join(Config.APP_DEST, fileRelative + '.js'),
         BUNDLER_OPTIONS)
         .catch((err: any) => done(err));
       return stream;
