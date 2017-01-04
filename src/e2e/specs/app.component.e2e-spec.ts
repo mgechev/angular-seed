08fefaec1499d421836e55ababc037ddf70c52c0
mgechev <mgechev@gmail.com>
2017-01-04 17:24:32 +0200

    fix: broken e2e test

--- src/e2e/specs/app.component.e2e-spec.ts
+++ src/e2e/specs/app.component.e2e-spec.ts
@@ -5,7 +5,7 @@ describe('App', () => {
   });
 
   it('should have a title', () => {
-    expect(browser.getTitle()).toEqual('Welcome to angular2-seed!');
+    expect(browser.getTitle()).toEqual('Welcome to angular-seed!');
   });
 
   it('should have <nav>', () => {
