561c32040cc63d2ff676f817ca0186773960ccd6
Eduardo Born <nosachamos@gmail.com>
2017-01-20 18:48:04 -0200

    feat(build): Another stab at having an rollup experimental build.

---
+++ tools/tasks/seed/build.bundles.app.rollup.exp.ts
@@ -0,0 +1,43 @@
+import Config from '../../config';
+import { writeFile } from 'fs';
+import { join } from 'path';
+
+const nodeResolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+const rollup = require('rollup');
+
+const config = {
+  entry: join(Config.TMP_DIR, Config.BOOTSTRAP_FACTORY_PROD_MODULE),
+  sourceMap: true,
+  treeshake: true,
+  moduleName: 'main',
+  plugins: [
+    nodeResolve({
+      jsnext: true, main: true, module: true
+    }),
+    commonjs({
+      include: 'node_modules/**'
+    })
+  ]
+};
+
+
+export = (done: any) => {
+  rollup.rollup(config)
+    .then((bundle: any) => {
+      var result = bundle.generate({
+        format: 'iife'
+      });
+      const path = join(Config.TMP_DIR, 'bundle.js');
+      writeFile(path, result.code, (error: any) => {
+        if (error) {
+          console.error(error);
+          process.exit(0);
+        }
+        done();
+      });
+    }, function (error: any) {
+      console.error(error);
+      process.exit(0);
+    });
+};
---
+++ tools/tasks/seed/transpile.bundles.rollup.exp.ts
@@ -0,0 +1,37 @@
+import * as gulp from 'gulp';
+import * as gulpLoadPlugins from 'gulp-load-plugins';
+import { join } from 'path';
+
+import Config from '../../config';
+import { makeTsProject,  } from '../../utils';
+import { TemplateLocalsBuilder } from '../../utils/seed/template_locals';
+
+const plugins = <any>gulpLoadPlugins();
+
+/**
+ * Executes the build process, transpiling the TypeScript files for the production environment.
+ */
+
+export = () => {
+  let tsProject = makeTsProject({
+    allowJs: true,
+    noFallthroughCasesInSwitch: false
+  }, Config.TMP_DIR);
+  let src = [
+    join(Config.TMP_DIR, 'bundle.js')
+  ];
+  let result = gulp.src(src)
+    .pipe(plugins.plumber())
+    .pipe(tsProject())
+    .once('error', function(e: any) {
+      this.once('finish', () => process.exit(1));
+    });
+
+  return result.js
+    .pipe(plugins.template(new TemplateLocalsBuilder().build()))
+    .pipe(plugins.rename(Config.JS_PROD_APP_BUNDLE))
+    .pipe(gulp.dest(Config.JS_DEST))
+    .on('error', (e: any) => {
+      console.log(e);
+    });
+};
