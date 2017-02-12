adb16dff8294372b6553b4dc9e220b7789826fcf
Yen-cheng Chen, 陳彥澄 <Shyam-Chen@users.noreply.github.com>
2017-02-12 19:42:17 +0800

    add google analytics

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -14,6 +14,7 @@ export class ProjectConfig extends SeedConfig {
   constructor() {
     super();
     // this.APP_TITLE = 'Put name of your app here';
+    // this.GOOGLE_ANALYTICS_ID = 'Your site's ID';
 
     /* Enable typeless compiler runs (faster) between typed compiler runs. */
     // this.TYPED_COMPILE_INTERVAL = 5;
