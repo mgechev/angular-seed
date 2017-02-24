504f714bc6171d8398edb39e3fe85828cdc008e0
Peter Pavlovich <pavlovich@gmail.com>
2017-02-24 01:33:00 -0500

    Correct type / misspelled function name: wihtoutStringifiedEnvConfig => withoutStringifiedEnvConfig

--- tools/tasks/seed/build.index.prod.ts
+++ tools/tasks/seed/build.index.prod.ts
@@ -16,7 +16,7 @@ export = () => {
   return gulp.src(join(Config.APP_SRC, 'index.html'))
     .pipe(injectJs())
     .pipe(injectCss())
-    .pipe(plugins.template(new TemplateLocalsBuilder().wihtoutStringifiedEnvConfig().build()))
+    .pipe(plugins.template(new TemplateLocalsBuilder().withoutStringifiedEnvConfig().build()))
     .pipe(gulp.dest(Config.APP_DEST));
 };
 
