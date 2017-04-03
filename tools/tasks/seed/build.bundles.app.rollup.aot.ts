de185e2188f5d354e1ad09d2016aed31af92017e
Melville <N0206010@LM.lmig.com>
2017-04-03 15:36:59 -0400

    provide rollup includes and named exports in project config

--- tools/tasks/seed/build.bundles.app.rollup.aot.ts
+++ tools/tasks/seed/build.bundles.app.rollup.aot.ts
@@ -22,12 +22,9 @@ const config = {
     nodeResolve({
       jsnext: true, main: true, module: true
     }),
-    commonjs({
-      include: 'node_modules/**',
-      namedExports: {
-        // 'node_modules/immutable/dist/immutable.js': [ 'Map', 'Set', 'List', 'fromJS' ],
-        // 'node_modules/ng2-dragula/ng2-dragula.js': [ 'DragulaModule', 'DragulaService' ]
-      }
+    commonjs({ //See project.config.ts to extend
+      include: Config.ROLLUP_INCLUDE_DIR,
+      namedExports: Config.getRollupNamedExports()
     })
   ]
 };
