59db153dde4224143a05e3dad37d442066346d5a
Minko Gechev <mgechev@gmail.com>
2017-05-08 07:54:43 -0700

    Revert "fix(tools): cache SCSS files only in prod build"

--- tools/tasks/seed/build.html_css.ts
+++ tools/tasks/seed/build.html_css.ts
@@ -85,7 +85,7 @@ function getSCSSFiles(cacheName:string, filesToCompile:string[], filesToExclude:
     filesToExclude.map((path:string) => { return '!' + path; })
   );
   return gulp.src(allFiles)
-    .pipe(isProd ? plugins.cached(cacheName) : plugins.util.noop())
+    .pipe(plugins.cached(cacheName))
     .pipe(plugins.progeny())
     .pipe(filter(filteredFiles));
 }
