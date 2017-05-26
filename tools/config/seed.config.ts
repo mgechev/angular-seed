e9993278f1f9300fca8f0f6977797a151a22b8f4
mgechev <mgechev@gmail.com>
2017-05-26 12:35:15 -0700

    build: fix test config

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -404,10 +404,10 @@ export class SeedConfig {
         'node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
       '@angular/router/testing': 'node_modules/@angular/router/bundles/router-testing.umd.js',
 
-      'app/*': '/app/*',
+      'app/': '/app/',
       // For test config
-      'dist/dev/*': '/base/dist/dev/*',
-      '*': 'node_modules/*'
+      'dist/dev/': '/base/dist/dev/',
+      '': 'node_modules/'
     },
     packages: {
       app: {
