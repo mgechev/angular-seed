42ab89fd9f14bc16df5cd8a69dcccc52084d6223
Felipe de Camargo Vieira <felipemitow@gmail.com>
2017-06-14 15:02:33 -0300

    adicionando pagina de teste

---
+++ src/client/app/restForm/restForm-routing.module.ts
@@ -0,0 +1,13 @@
+﻿import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { restFormComponent } from './restForm.component';
+
+@NgModule({
+  imports: [
+    RouterModule.forChild([
+          { path: 'restform', component: restFormComponent }
+    ])
+  ],
+  exports: [RouterModule]
+})
+export class restFormRoutingModule { }
---
+++ src/client/app/restForm/restForm.module.ts
@@ -0,0 +1,13 @@
+﻿import { NgModule } from '@angular/core';
+import { restFormComponent } from './restForm.component';
+import { restFormRoutingModule } from './restForm-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { NameListService } from '../shared/name-list/name-list.service';
+
+@NgModule({
+    imports: [restFormRoutingModule, SharedModule],
+  declarations: [restFormComponent],
+  exports: [restFormComponent],
+  providers: [NameListService]
+})
+export class RestFormModule { }
