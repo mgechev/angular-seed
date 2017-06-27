cc5dd846e909124a9c6db5fe1dbaa139946dbd42
Minko Gechev <mgechev@gmail.com>
2017-06-28 02:24:09 +0300

    Revert "Postinstall script update"

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -334,7 +334,8 @@ export class SeedConfig {
     { src: 'zone.js/dist/long-stack-trace-zone.js', inject: 'libs', buildType: BUILD_TYPES.DEVELOPMENT },
     { src: 'intl/dist/Intl.min.js', inject: 'shims' },
     { src: 'systemjs/dist/system.src.js', inject: 'shims', buildType: BUILD_TYPES.DEVELOPMENT },
-    { src: 'rxjs/Rx.js', inject: 'libs', buildType: BUILD_TYPES.DEVELOPMENT },
+    // Temporary fix. See https://github.com/angular/angular/issues/9359
+    { src: '.tmp/Rx.min.js', inject: 'libs', buildType: BUILD_TYPES.DEVELOPMENT },
   ];
 
   /**
