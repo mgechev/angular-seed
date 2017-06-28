f9effae982071d6852f9bcd047c68c0b43c755f9
RoxKilly <17jktr+9b0svtcym5900@sharklasers.com>
2017-06-27 22:54:39 -0400

    Revert "Add initialize and finalize tasks to all builds"
    This reverts commit e4ec718df35f63a151edc6ec87f88e712bb58e13.

--- tools/tasks/project/initialize.ts
+++
@@ -1,8 +0,0 @@
-/**
- * This task is run at the start of every build (see /tools/config/seed.tasks.json)
- * The task is currently inert (does nothing).
- * Replace this inert task with your own if you wish to run the same startup script before every build
- */
-export = (done: any) => {
-  done();
-};
