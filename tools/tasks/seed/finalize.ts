b1a392fca5c9e7f03c389d03123f259a3e45bb97
RoxKilly <17jktr+9b0svtcym5900@sharklasers.com>
2017-06-27 22:54:39 -0400

    Revert "Add initialize and finalize tasks to all builds"
    This reverts commit e4ec718df35f63a151edc6ec87f88e712bb58e13.

--- tools/tasks/seed/finalize.ts
+++
@@ -1,8 +0,0 @@
-/**
- * This task is run at the end of every build (see /tools/config/seed.tasks.json)
- * The task is currently inert (does nothing).
- * You can replace it with your own shutdown task by putting a file at /tools/tasks/project/finalize.ts
- */
-export = (done: any) => {
-    done();
-};
--- tools/tasks/seed/initialize.ts
+++
@@ -1,8 +0,0 @@
-/**
- * This task is run at the start of every build (see /tools/config/seed.tasks.json)
- * The task is currently inert (does nothing).
- * You can replace it with your own initialization task by putting a file at /tools/tasks/project/initialize.ts
- */
-export = (done: any) => {
-    done();
-};
