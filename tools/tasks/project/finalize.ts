94ace7795b06e463233b984ede48755c8426989a
RoxKilly <17jktr+9b0svtcym5900@sharklasers.com>
2017-06-27 20:06:24 -0400

    Add initialize and finalize tasks to all builds
    All build sequences now begin by running the "initialize" task, and
    end by running the "finalize" task. Users now have a convenient way
    to run a startup/shutdown script before/after all builds without having
    to override the composite tasks in /tools/config/seed.config.json.
    
    This should make upgrading simpler.

---
+++ tools/tasks/project/finalize.ts
@@ -0,0 +1,8 @@
+/**
+ * This task is run at the end of every build (see /tools/config/seed.tasks.json)
+ * The task is currently inert (does nothing).
+ * Replace this inert task with your own if you wish to run the same shutdown script after every build
+ */
+export = (done: any) => {
+  done();
+};
---
+++ tools/tasks/seed/initialize.ts
@@ -0,0 +1,8 @@
+/**
+ * This task is run at the start of every build (see /tools/config/seed.tasks.json)
+ * The task is currently inert (does nothing).
+ * You can replace it with your own initialization task by putting a file at /tools/tasks/project/initialize.ts
+ */
+export = (done: any) => {
+    done();
+};
