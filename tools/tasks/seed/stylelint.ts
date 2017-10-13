4303c6c0c177e62c029fbf47026ad3e2bbe47ce9
Francesco Colamonici <f.colamonici@gmail.com>
2017-10-13 09:19:13 +0200

    stylelint setup

---
+++ tools/tasks/seed/stylelint.ts
@@ -0,0 +1,25 @@
+import * as gulp from 'gulp';
+import * as gulpLoadPlugins from 'gulp-load-plugins';
+import { join } from 'path';
+
+import Config from '../../config';
+
+const plugins = <any>gulpLoadPlugins();
+
+/**
+ * Executes the build process, linting the TypeScript files using `codelyzer`.
+ */
+export = () => {
+  const src = [
+    join(Config.APP_SRC, '**/*.css'),
+    join(Config.APP_SRC, '**/*.scss'),
+  ];
+
+  return gulp.src(src, { 'base': '.' })
+    .pipe(plugins.stylelint({
+      failAfterError: require('is-ci') || Config.FORCE_STYLELINT_EMIT_ERROR,
+      reporters: [
+        { formatter: 'string', console: true }
+      ]
+    }));
+};
