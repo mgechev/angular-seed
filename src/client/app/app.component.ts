6d7439570ad02d6d470a7bf8411b65fe1a9b158b
Samet Alemdar <sa.alemdar@hotmail.com>
2018-04-07 02:55:13 +0200

    drop usages of operators.ts

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Config } from './shared/config/env.config';
-import './operators';
 
 /**
  * This class represents the main application component.
--- tools/tasks/seed/build.bundle.rxjs.ts
+++ tools/tasks/seed/build.bundle.rxjs.ts
@@ -18,7 +18,7 @@ export = (done: any) => {
     paths: {
       'n:*': 'node_modules/*',
       'rxjs/*': 'node_modules/rxjs/*.js',
-      'rxjs/operators': 'node_modules/rxjs/operators.js'
+      'rxjs/operators/*': 'node_modules/rxjs/*'
     },
     map: {
       'rxjs': 'n:rxjs',
