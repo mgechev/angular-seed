8b08e18b426661a578ab020e3850c4b5cf3a16aa
Samet Alemdar <sa.alemdar@hotmail.com>
2018-04-07 02:37:56 +0200

    drop operators.ts

--- src/client/app/operators.ts
+++
@@ -1,7 +0,0 @@
-// rxjs: Add only if lettable operators are being used. Pipeable operators don't require this.
-// import 'rxjs/add/observable/throw';
-// import 'rxjs/add/operator/map';
-// import 'rxjs/add/operator/catch';
-
-// ngrx
-//import '@ngrx/core/add/operator/select';
