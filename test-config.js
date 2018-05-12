8c535082e6518a967dde89826a0b15b92bd840f0
Jay <yajra.b.eroble@gmail.com>
2018-05-12 23:40:56 +0800

    add bundle rxjs for less request (#2271)
    * add bundle rxjs for less request
    
    * updating the test config and karma config

--- test-config.js
+++ test-config.js
@@ -8,7 +8,24 @@ System.config({
     '': {
       defaultExtension: 'js'
     },
-    rxjs: {
+    'rxjs': {
+      main: 'index.js',
+      defaultExtension: 'js'
+    },
+    'rxjs/ajax': {
+      main: 'index.js',
+      defaultExtension: 'js'
+    },
+    'rxjs/operators': {
+      main: 'index.js',
+      defaultExtension: 'js'
+    },
+    'rxjs/testing': {
+      main: 'index.js',
+      defaultExtension: 'js'
+    },
+    'rxjs/webSocket': {
+      main: 'index.js',
       defaultExtension: 'js'
     }
   }
