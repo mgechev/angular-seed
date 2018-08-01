3ba7ae2f3f7377c4270a7effe03d53d584261a86
Isaac Hookom <isaac.hookom@gmail.com>
2018-08-01 13:55:59 -0400

    database export

---
+++ db_insert.js
@@ -0,0 +1,26 @@
+const Datastore = require('@google-cloud/datastore');
+const fs = require('fs');
+
+let datastore = new Datastore({
+    projectId: 'climb-on-the-way-1487535946897'
+});
+
+var lines = fs.readFileSync('db_rows','utf8').split('\n');
+
+lines.forEach(row => {
+    let cols = row.split(', ');
+
+    let data = {
+        name: cols[0].substr(1, cols[0].length - 2), 
+        lat: parseFloat(cols[1]),
+        long: parseFloat(cols[2])
+    };
+
+    if (data.name === '') {
+     return;
+    }
+    
+    let key = datastore.key(['Location', data.name+'-'+data.season]);
+    datastore.insert({ key, data });
+})
+  
