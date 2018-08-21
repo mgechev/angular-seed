e9305fd93456afb9d1cdd6be791ec47fb574aa98
Arnaud Peloquin <arnaud.peloquin@abolis.fr>
2018-08-21 11:37:38 +0200

    Improve production docker build - make better use of layer cache
    There is no need to bust the "npm install" cache layer if only the app
    source files have changed. This only applies to the prod image.

--- .docker/rm.optional.deps.js
+++ .docker/rm.optional.deps.js
@@ -10,13 +10,3 @@ const fs = require('fs');
 const npmPackage = JSON.parse(fs.readFileSync('package.json'));
 delete npmPackage['optionalDependencies'];
 fs.writeFileSync('package.json', JSON.stringify(npmPackage, null, 2));
-
-/**
- * Todo is there a better way to handdle optional types in tsconfig.json ?
- */
-let tsConfigJson = 'src/client/tsconfig.json';  // only copied in the prod image
-if (fs.existsSync(tsConfigJson)) {
-  const tsConfig = JSON.parse(fs.readFileSync(tsConfigJson));
-  tsConfig['compilerOptions']['types'] = tsConfig['compilerOptions']['types'].filter(t => t !== 'jasmine');
-  fs.writeFileSync(tsConfigJson, JSON.stringify(tsConfig, null, 2));
-}
