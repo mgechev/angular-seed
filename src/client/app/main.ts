8ac702b66fe29201303e606e3e7bf2cf4d79fa35
Aamir Shah <syedaamir974@gmail.com>
2018-11-17 00:39:27 +0500

    Register service worker via `ServiceWorkerModule`. (#2365)

--- src/client/app/main.ts
+++ src/client/app/main.ts
@@ -21,15 +21,3 @@ if (String('<%= BUILD_TYPE %>') === 'prod') {
 // const options: any = { providers };
 platformBrowserDynamic().bootstrapModule(AppModule /*, options*/);
 // });
-
-// In order to start the Service Worker located at "/ngsw-worker.js"
-// uncomment this line. More about Service Workers here
-// https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
-//
-// if ('serviceWorker' in navigator) {
-//   const workerScript = '/ngsw-worker.js';
-//   (<any>navigator).serviceWorker
-//     .register(workerScript)
-//     .then((registration: any) => console.log('ServiceWorker registration successful with scope: ', registration.scope))
-//     .catch((err: any) => console.log('ServiceWorker registration failed: ', err));
-// }
